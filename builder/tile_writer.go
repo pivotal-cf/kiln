29077d9feaac139b721c74956066c22d1c7a3471
Michelle He <mhe@gmail.com>
2017-11-10 21:45:29 -0800

    kiln adds the right js migration files to .pivotal
    [#152705046]

--- builder/tile_writer.go
+++ builder/tile_writer.go
@@ -171,6 +171,12 @@ func (w TileWriter) addMigrations(migrationsDir []string, outputFile string) err
 
 	for _, migrationDir := range migrationsDir {
 		err := w.filesystem.Walk(migrationDir, func(filePath string, info os.FileInfo, err error) error {
+			isTest, _ := regexp.MatchString(`tests\/.+_test.js$`, filePath)
+			isJsFile, _ := regexp.MatchString(`.js$`, filePath)
+			if isTest || !isJsFile {
+				return nil
+			}
+
 			if err != nil {
 				return err
 			}
