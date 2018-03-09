c3b5179391264c5a6b05c78d88f1148854eaccd0
Genevieve L'Esperance <glesperance@pivotal.io>
2018-03-08 16:04:37 -0800

    Backfill collection blueprint property test.
    Signed-off-by: Ryan Moran <rmoran@pivotal.io>

---
+++ internal/proofing/collection_property_blueprint_test.go
@@ -0,0 +1,43 @@
+package proofing_test
+
+import (
+	"github.com/pivotal-cf/kiln/internal/proofing"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("CollectionPropertyBlueprint", func() {
+	var collectionPropertyBlueprint proofing.CollectionPropertyBlueprint
+
+	BeforeEach(func() {
+		productTemplate, err := proofing.Parse("fixtures/property_blueprints.yml")
+		Expect(err).NotTo(HaveOccurred())
+
+		var ok bool
+		collectionPropertyBlueprint, ok = productTemplate.PropertyBlueprints[2].(proofing.CollectionPropertyBlueprint)
+		Expect(ok).To(BeTrue())
+	})
+
+	It("parses their structure", func() {
+		Expect(collectionPropertyBlueprint.Name).To(Equal("some-name"))
+		Expect(collectionPropertyBlueprint.Type).To(Equal("collection"))
+		Expect(collectionPropertyBlueprint.Default).To(Equal("some-default"))
+		Expect(collectionPropertyBlueprint.Constraints.Min).To(Equal(5))
+		Expect(collectionPropertyBlueprint.Options).To(HaveLen(1))
+		Expect(collectionPropertyBlueprint.Configurable).To(BeTrue())
+		Expect(collectionPropertyBlueprint.Optional).To(BeTrue())
+		Expect(collectionPropertyBlueprint.FreezeOnDeploy).To(BeTrue())
+		Expect(collectionPropertyBlueprint.Unique).To(BeTrue())
+		Expect(collectionPropertyBlueprint.ResourceDefinitions).To(HaveLen(1))
+	})
+
+	Context("options", func() {
+		It("parses their structure", func() {
+			option := collectionPropertyBlueprint.Options[0]
+
+			Expect(option.Label).To(Equal("some-label"))
+			Expect(option.Name).To(Equal("some-name"))
+		})
+	})
+})
