f301d8e60d8a42bf13f2114530491d8740306498
Kalai Wei <kawei@pivotal.io>
2018-03-22 18:10:28 -0700

    Adds some starting validations for releases
    Signed-off-by: Ryan Moran <rmoran@pivotal.io>

--- internal/proofing/release.go
+++ internal/proofing/release.go
@@ -1,5 +1,10 @@
 package proofing
 
+import (
+	"fmt"
+	"strings"
+)
+
 type Release struct {
 	Name    string `yaml:"name"`
 	Version string `yaml:"version"`
@@ -8,4 +13,26 @@ type Release struct {
 	SHA1 string `yaml:"sha1"` // NOTE: this only exists because of kiln
 
 	// TODO: validations: https://github.com/pivotal-cf/installation/blob/039a2ef3f751ef5915c425da8150a29af4b764dd/web/app/models/persistence/metadata/release.rb#L8-L15
+}
+
+type CompoundError []error
+
+func (ce *CompoundError) Add(err error) {
+	*ce = append(*ce, err)
+}
+
+func (ce *CompoundError) Error() string {
+	var messages []string
+	for _, e := range *ce {
+		messages = append(messages, fmt.Sprintf("- %s", e))
+	}
+	return strings.Join(messages, "\n")
+}
+
+func (r Release) Validate() error {
+	var err error
+	err = ValidatePresence(err, r, "Name")
+	err = ValidatePresence(err, r, "File")
+	err = ValidatePresence(err, r, "Version")
+	return err
 }
--- internal/proofing/release_test.go
+++ internal/proofing/release_test.go
@@ -23,4 +23,41 @@ var _ = Describe("Release", func() {
 		Expect(release.SHA1).To(Equal("some-sha1"))
 		Expect(release.Version).To(Equal("some-version"))
 	})
+
+	It("is valid", func() {
+		Expect(release.Validate()).To(Succeed())
+	})
+
+	Context("validations", func() {
+		BeforeEach(func() {
+			release = proofing.Release{
+				Name:    "some-name",
+				Version: "some-version",
+				File:    "some-file",
+				SHA1:    "some-sha1",
+			}
+		})
+
+		It("validates the presence of the Name field", func() {
+			release.Name = ""
+			Expect(release.Validate()).To(MatchError("release name must be present"))
+		})
+
+		It("validates the presence of the File field", func() {
+			release.File = ""
+			Expect(release.Validate()).To(MatchError("release file must be present"))
+		})
+
+		It("validates the presence of the Name field", func() {
+			release.Version = ""
+			Expect(release.Validate()).To(MatchError("release version must be present"))
+		})
+
+		It("combines validations", func() {
+			release = proofing.Release{}
+			Expect(release.Validate()).To(MatchError(`- release name must be present
+- release file must be present
+- release version must be present`))
+		})
+	})
 })
