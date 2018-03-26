c8dbfab2b5be032e1a52b16bdbdb76d7e600840e
Ryan Moran <rmoran@pivotal.io>
2018-03-26 15:03:22 -0700

    Factors out some small methods for generating a cargo manifest

--- internal/cargo/generator.go
+++ internal/cargo/generator.go
@@ -19,69 +19,11 @@ func NewGenerator() Generator {
 }
 
 func (g Generator) Execute(template proofing.ProductTemplate, config OpsManagerConfig) Manifest {
-	var releases []Release
-	for _, release := range template.Releases {
-		releases = append(releases, Release{
-			Name:    release.Name,
-			Version: release.Version,
-		})
-	}
-
-	var stemcell Stemcell
-	for _, boshStemcell := range config.Stemcells {
-		if boshStemcell.OS == template.StemcellCriteria.OS {
-			if boshStemcell.Version == template.StemcellCriteria.Version {
-				stemcell = Stemcell{
-					Alias:   boshStemcell.Name,
-					OS:      boshStemcell.OS,
-					Version: boshStemcell.Version,
-				}
-			}
-		}
-	}
-
-	update := Update{
-		Canaries:        1,
-		CanaryWatchTime: "30000-300000",
-		UpdateWatchTime: "30000-300000",
-		MaxInFlight:     1,
-		MaxErrors:       2,
-		Serial:          template.Serial,
-	}
-
-	var instanceGroups []InstanceGroup
-	for _, jobType := range template.JobTypes {
-		lifecycle := "service"
-		if jobType.Errand {
-			lifecycle = "errand"
-		}
-
-		instances := jobType.InstanceDefinition.Default
-		for _, resourceConfig := range config.ResourceConfigs {
-			if resourceConfig.Name == jobType.Name {
-				if !resourceConfig.Instances.IsAutomatic() {
-					instances = resourceConfig.Instances.Value
-				}
-			}
-		}
-
-		instanceGroups = append(instanceGroups, InstanceGroup{
-			Name:      jobType.Name,
-			AZs:       config.AvailabilityZones,
-			Lifecycle: lifecycle,
-			Stemcell:  stemcell.Alias,
-			Instances: instances,
-		})
-	}
-
-	var variables []Variable
-	for _, variable := range template.Variables {
-		variables = append(variables, Variable{
-			Name:    variable.Name,
-			Options: variable.Options,
-			Type:    variable.Type,
-		})
-	}
+	releases := generateReleases(template.Releases)
+	stemcell := findStemcell(template.StemcellCriteria, config.Stemcells)
+	update := generateUpdate(template.Serial)
+	instanceGroups := generateInstanceGroups(template.JobTypes, config.ResourceConfigs, config.AvailabilityZones, stemcell.Alias)
+	variables := generateVariables(template.Variables)
 
 	return Manifest{
 		Name:           config.DeploymentName,
@@ -91,4 +33,90 @@ func (g Generator) Execute(template proofing.ProductTemplate, config OpsManagerC
 		Variables:      variables,
 		InstanceGroups: instanceGroups,
 	}
+}
+
+func generateReleases(templateReleases []proofing.Release) []Release {
+	var releases []Release
+
+	for _, release := range templateReleases {
+		releases = append(releases, Release{
+			Name:    release.Name,
+			Version: release.Version,
+		})
+	}
+
+	return releases
+}
+
+func findStemcell(criteria proofing.StemcellCriteria, stemcells []opsman.Stemcell) Stemcell {
+	var stemcell Stemcell
+
+	for _, s := range stemcells {
+		if s.OS == criteria.OS {
+			if s.Version == criteria.Version {
+				stemcell = Stemcell{
+					Alias:   s.Name,
+					OS:      s.OS,
+					Version: s.Version,
+				}
+			}
+		}
+	}
+
+	return stemcell
+}
+
+func generateUpdate(serial bool) Update {
+	return Update{
+		Canaries:        1,
+		CanaryWatchTime: "30000-300000",
+		UpdateWatchTime: "30000-300000",
+		MaxInFlight:     1,
+		MaxErrors:       2,
+		Serial:          serial,
+	}
+}
+
+func generateInstanceGroups(jobTypes []proofing.JobType, resourceConfigs []opsman.ResourceConfig, availabilityZones []string, stemcellAlias string) []InstanceGroup {
+	var instanceGroups []InstanceGroup
+
+	for _, jobType := range jobTypes {
+		lifecycle := "service"
+		if jobType.Errand {
+			lifecycle = "errand"
+		}
+
+		instances := jobType.InstanceDefinition.Default
+		for _, resourceConfig := range resourceConfigs {
+			if resourceConfig.Name == jobType.Name {
+				if !resourceConfig.Instances.IsAutomatic() {
+					instances = resourceConfig.Instances.Value
+				}
+			}
+		}
+
+		instanceGroups = append(instanceGroups, InstanceGroup{
+			Name:      jobType.Name,
+			AZs:       availabilityZones,
+			Lifecycle: lifecycle,
+			Stemcell:  stemcellAlias,
+			Instances: instances,
+		})
+	}
+
+	return instanceGroups
+}
+
+func generateVariables(templateVariables []proofing.Variable) []Variable {
+	var variables []Variable
+
+	for _, variable := range templateVariables {
+		variables = append(variables, Variable{
+			Name:    variable.Name,
+			Options: variable.Options,
+			Type:    variable.Type,
+		})
+	}
+
+	return variables
 }
