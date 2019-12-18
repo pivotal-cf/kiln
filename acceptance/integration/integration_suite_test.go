7d4f916edac0304eaae78c8f28bebd46427a2e84
Nat Bennett <nbennett@pivotal.io>
2019-12-18 11:07:59 -0800

    Separate out tests that integrate with network services
    - The tests in acceptance/integration all talk to the internet (either
      PivNet or S3). This makes them slow and flakey.
    
    - To run these tests, set RUN_INTEGRATION_TESTS="true"
    
    Co-authored-by: Nat Bennett <nbennett@pivotal.io>
    Co-authored-by: Matt Royal <mroyal@pivotal.io>
    
    [#170218474] Pipestat: Review and close tasks in bugs/incidents in the delivery tracker

---
+++ acceptance/integration/integration_suite_test.go
@@ -0,0 +1,33 @@
+package integration_test
+
+import (
+	"github.com/onsi/gomega/gexec"
+	"os"
+	"testing"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var pathToMain string
+
+func TestIntegration(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "acceptance/integration")
+}
+
+var _ = BeforeSuite(func() {
+	var err error
+	pathToMain, err = gexec.Build("github.com/pivotal-cf/kiln")
+	Expect(err).NotTo(HaveOccurred())
+})
+
+var _ = AfterSuite(func() {
+	gexec.CleanupBuildArtifacts()
+})
+
+var _ = BeforeEach(func() {
+	if os.Getenv("RUN_INTEGRATION_TESTS") != "true" {
+		Skip("Skipping integration tests. Set RUN_INTEGRATION_TESTS=\"true\" to run them")
+	}
+})
