d94177142c11f4bb8a1c472f28cdba175a36cfca
Christopher Hunter <hunterch@vmware.com>
2021-09-30 21:42:53 -0700

    release version in lockfile must match constraint

--- internal/commands/validate_test.go
+++ internal/commands/validate_test.go
@@ -1,6 +1,7 @@
 package commands
 
 import (
+	"github.com/pivotal-cf/kiln/pkg/cargo"
 	"testing"
 
 	"github.com/go-git/go-billy/v5/osfs"
@@ -55,4 +56,81 @@ func TestValidate_PinnedRelease(t *testing.T) {
 	}
 	err := validate.Execute(nil)
 	please.Expect(err).NotTo(Ω.HaveOccurred())
+}
+
+func TestValidate_validateRelease(t *testing.T) {
+	t.Run("missing name", func(t *testing.T) {
+		please := Ω.NewWithT(t)
+		r := cargo.ReleaseKiln{}
+		l := cargo.ReleaseLock{}
+		err := validateRelease(r, l, 0)
+		please.Expect(err).To(Ω.And(
+			Ω.HaveOccurred(),
+			Ω.MatchError(Ω.ContainSubstring("missing name")),
+		))
+	})
+
+	t.Run("no version", func(t *testing.T) {
+		please := Ω.NewWithT(t)
+		r := cargo.ReleaseKiln{
+			Name: "capi",
+		}
+		l := cargo.ReleaseLock{
+			Name: "capi",
+			Version: "2.3.4",
+		}
+		err := validateRelease(r, l, 0)
+		please.Expect(err).NotTo(Ω.HaveOccurred())
+	})
+
+	t.Run("invalid version constraint", func(t *testing.T) {
+		please := Ω.NewWithT(t)
+		r := cargo.ReleaseKiln{
+			Name: "capi",
+			Version: "meh",
+		}
+		l := cargo.ReleaseLock{
+			Name: "capi",
+			Version: "2.3.4",
+		}
+		err := validateRelease(r, l, 0)
+		please.Expect(err).To(Ω.And(
+			Ω.HaveOccurred(),
+			Ω.MatchError(Ω.ContainSubstring("invalid version constraint")),
+		))
+	})
+
+	t.Run("version does not match constraint", func(t *testing.T) {
+		please := Ω.NewWithT(t)
+		r := cargo.ReleaseKiln{
+			Name: "capi",
+			Version: "~2",
+		}
+		l := cargo.ReleaseLock{
+			Name: "capi",
+			Version: "3.0.5",
+		}
+		err := validateRelease(r, l, 0)
+		please.Expect(err).To(Ω.And(
+			Ω.HaveOccurred(),
+			Ω.MatchError(Ω.ContainSubstring("match constraint")),
+		))
+	})
+
+	t.Run("invalid lock version", func(t *testing.T) {
+		please := Ω.NewWithT(t)
+		r := cargo.ReleaseKiln{
+			Name: "capi",
+			Version: "~2",
+		}
+		l := cargo.ReleaseLock{
+			Name: "capi",
+			Version: "BAD",
+		}
+		err := validateRelease(r, l, 0)
+		please.Expect(err).To(Ω.And(
+			Ω.HaveOccurred(),
+			Ω.MatchError(Ω.ContainSubstring("invalid lock version")),
+		))
+	})
 }
