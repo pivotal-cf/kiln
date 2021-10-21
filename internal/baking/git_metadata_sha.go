12f8de3261acc5e37b614198a98c90dc50d7b004
Christopher Hunter <hunterch@vmware.com>
2021-10-20 17:18:46 -0700

    use local gitignores when checking if the state is clean

--- internal/baking/git_metadata_sha.go
+++ internal/baking/git_metadata_sha.go
@@ -2,7 +2,13 @@ package baking
 
 import (
 	"errors"
+	"io/fs"
+	"io/ioutil"
+	"path/filepath"
+	"strings"
+
 	"github.com/go-git/go-git/v5"
+	"github.com/go-git/go-git/v5/plumbing/format/gitignore"
 )
 
 func GitMetadataSha(p string, allowDirty bool) (string, error) {
@@ -16,6 +22,12 @@ func GitMetadataSha(p string, allowDirty bool) (string, error) {
 		if err != nil {
 			return "", nil
 		}
+
+		excludes, err := getExcludePatterns(wt.Filesystem.Root())
+		if err != nil {
+			return "", err
+		}
+		wt.Excludes = excludes
 
 		status, err := wt.Status()
 		if err != nil {
@@ -33,4 +45,32 @@ func GitMetadataSha(p string, allowDirty bool) (string, error) {
 	}
 
 	return head.Hash().String(), nil
+}
+
+func getExcludePatterns(p string) ([]gitignore.Pattern, error) {
+	var ignored []gitignore.Pattern
+
+	parsePatterns := func(pth string) error {
+		buf, err := ioutil.ReadFile(pth)
+		if err != nil {
+			return err
+		}
+		lines := strings.Split(string(buf), "\n")
+		for i := range lines {
+			ignored = append(ignored, gitignore.ParsePattern(lines[i], strings.Split(pth, string(filepath.Separator))))
+		}
+		return nil
+	}
+
+	// TODO: read git config --global core.excludesfile
+
+	return ignored, filepath.Walk(p, func(pth string, info fs.FileInfo, err error) error {
+		if info.IsDir() {
+			return nil
+		}
+		if info.Name() != ".gitignore" {
+			return nil
+		}
+		return parsePatterns(pth)
+	})
 }
