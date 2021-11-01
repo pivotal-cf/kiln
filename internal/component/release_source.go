0ab209fb5d963bcb7e06f7c501f87d25b16b53f9
Christopher Hunter <hunterch@vmware.com>
2021-11-01 10:41:16 -0700

    docs: add comments for exported release source stuff
    Authored-by: Christopher Hunter <hunterch@vmware.com>

--- internal/component/release_source.go
+++ internal/component/release_source.go
@@ -7,7 +7,9 @@ import (
 	"log"
 )
 
-//counterfeiter:generate -o ./fakes/multi_release_source.go --fake-name MultiReleaseSource . MultiReleaseSource
+
+// MultiReleaseSource wraps a set of release sources. It is mostly used to generate fakes
+// for testing commands. See ReleaseSourceList for the concrete implementation.
 type MultiReleaseSource interface {
 	GetMatchedRelease(Requirement) (Lock, bool, error)
 	FindReleaseVersion(Requirement) (Lock, bool, error)
@@ -15,34 +17,62 @@ type MultiReleaseSource interface {
 
 	FindByID(string) (ReleaseSource, error)
 }
+//counterfeiter:generate -o ./fakes/multi_release_source.go --fake-name MultiReleaseSource . MultiReleaseSource
 
-//counterfeiter:generate -o ./fakes/release_uploader.go --fake-name ReleaseUploader . ReleaseUploader
+// ReleaseUploader represents a place to put releases. Some implementations of ReleaseSource
+// should implement this interface. Credentials for this should come from an interpolated
+// cargo.ReleaseSourceConfig.
 type ReleaseUploader interface {
 	GetMatchedRelease(Requirement) (Lock, bool, error)
 	UploadRelease(spec Requirement, file io.Reader) (Lock, error)
 }
+//counterfeiter:generate -o ./fakes/release_uploader.go --fake-name ReleaseUploader . ReleaseUploader
 
-//counterfeiter:generate -o ./fakes/remote_pather.go --fake-name RemotePather . RemotePather
+// RemotePather is used to get the remote path for a remote release. For example
+// the complete s3 uri.
+//
+// This interface may be ripe for removal.
 type RemotePather interface {
 	RemotePath(Requirement) (string, error)
 }
+//counterfeiter:generate -o ./fakes/remote_pather.go --fake-name RemotePather . RemotePather
 
-//counterfeiter:generate -o ./fakes/release_source.go --fake-name ReleaseSource . ReleaseSource
+// ReleaseSource represents a source where a tile component BOSH releases may come from.
+// The releases may be compiled or just built bosh releases.
 type ReleaseSource interface {
+	// Configuration returns the configuration of the ReleaseSource that came from the kilnfile.
+	// It should not be modified.
 	Configuration() cargo.ReleaseSourceConfig
 
+	// GetMatchedRelease uses the Name and Version and if supported StemcellOS and StemcellVersion
+	// fields on Requirement to download a specific release.
 	GetMatchedRelease(Requirement) (Lock, bool, error)
+
+	// FindReleaseVersion may use any of the fields on Requirement to return the best matching
+	// release.
 	FindReleaseVersion(Requirement) (Lock, bool, error)
+
+	// DownloadRelease downloads the release and writes the resulting file to the releasesDir.
+	// It should also calculate and set the SHA1 field on the Local result; it does not need
+	// to ensure the sums match, the caller must verify this.
 	DownloadRelease(releasesDir string, remoteRelease Lock, downloadThreads int) (Local, error)
 }
+//counterfeiter:generate -o ./fakes/release_source.go --fake-name ReleaseSource . ReleaseSource
 
 const (
 	panicMessageWrongReleaseSourceType = "wrong constructor for release source configuration"
 
+	// ReleaseSourceTypeBOSHIO is the value of the Type field on cargo.ReleaseSourceConfig
+	// for fetching https://bosh.io releases.
 	ReleaseSourceTypeBOSHIO = "bosh.io"
+
+	// ReleaseSourceTypeS3 is the value for the Type field on cargo.ReleaseSourceConfig
+	// for releases stored on
 	ReleaseSourceTypeS3     = "s3"
 )
 
+// ReleaseSourceFactory returns a configured ReleaseSource based on the Type field on the
+// cargo.ReleaseSourceConfig structure.
 func ReleaseSourceFactory(releaseConfig cargo.ReleaseSourceConfig, outLogger *log.Logger) ReleaseSource {
 	switch releaseConfig.Type {
 	case ReleaseSourceTypeBOSHIO:
