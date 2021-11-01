7643c1df513890210b6ff57886f8dc10444501ab
goreleaserbot <goreleaser@carlosbecker.com>
2021-11-01 21:48:59 +0000

    Brew formula update for kiln version 0.62.0

--- HomebrewFormula/kiln.rb
+++ HomebrewFormula/kiln.rb
@@ -5,24 +5,28 @@
 class Kiln < Formula
   desc ""
   homepage ""
-  version "0.0.0-dev-pr265.5"
+  version "0.62.0"
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/pivotal-cf/kiln/releases/download/0.0.0-dev-pr265.5/kiln-darwin-0.0.0-dev-pr265.5.tar.gz"
-      sha256 "72bb99897c2593fb062c0749133a0369c1000b64b12ea835b61044490b120abc"
+      url "https://github.com/pivotal-cf/kiln/releases/download/0.62.0/kiln-darwin-0.62.0.tar.gz"
+      sha256 "e46edb3f5d1722ef85e613ef45258e78ba956301774dd65cd06d43afaf91fff0"
+
+      def install
+        bin.install "kiln"
+      end
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/pivotal-cf/kiln/releases/download/0.0.0-dev-pr265.5/kiln-linux-0.0.0-dev-pr265.5.tar.gz"
-      sha256 "f29dbae60841fa46e2b062ae41b20261d83b811df24e9ca6d2bfb5476ae530b2"
-    end
-  end
+      url "https://github.com/pivotal-cf/kiln/releases/download/0.62.0/kiln-linux-0.62.0.tar.gz"
+      sha256 "793fcbf3ce55db04cca766e98fdf749424bea525c770c7cd92d5cbb8dcdc8828"
 
-  def install
-    bin.install "kiln"
+      def install
+        bin.install "kiln"
+      end
+    end
   end
 
   test do
