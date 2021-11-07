e59a25ca8e49ef690e76d200245849e96c77202f
ciriarte <ciriarte@users.noreply.github.com>
2021-11-07 14:09:29 -0800

    refactor: improve logging details
    Authored-by: Carlos Iriarte <ciriarte@users.noreply.github.com>

--- internal/commands/cache_compiled_releases.go
+++ internal/commands/cache_compiled_releases.go
@@ -184,7 +184,7 @@ func (cmd CacheCompiledReleases) Execute(args []string) error {
 
 		newRemote, err := cmd.cacheRelease(bosh, bucket, deployment, requirement, osVersionSlug)
 		if err != nil {
-			fmt.Printf("failed to cache release %s: %s", rel.Name, err)
+			cmd.Logger.Printf("\tfailed to cache release %s/%s for %s: %s\n", rel.Name, rel.Version, osVersionSlug, err)
 			continue
 		}
 
