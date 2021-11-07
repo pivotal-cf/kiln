6d15d50639851c9160273d204bbe1a07a961878f
ciriarte <ciriarte@users.noreply.github.com>
2021-11-07 14:09:29 -0800

    refactor: improve logging details
    Authored-by: Carlos Iriarte <ciriarte@users.noreply.github.com>

--- internal/component/release_source_list.go
+++ internal/component/release_source_list.go
@@ -117,13 +117,11 @@ func NewMultiReleaseSource(sources ...ReleaseSource) ReleaseSourceList {
 
 func (list ReleaseSourceList) GetMatchedRelease(requirement Requirement) (Lock, bool, error) {
 	for _, src := range list {
-		fmt.Printf("checking %s %s in %s", requirement.Name, requirement.Version, src.Configuration().ID)
 		rel, found, err := src.GetMatchedRelease(requirement)
 		if err != nil {
 			return Lock{}, false, scopedError(src.Configuration().ID, err)
 		}
 		if found {
-			fmt.Printf("found %s %s in %s", requirement.Name, requirement.Version, src.Configuration().ID)
 			return rel, true, nil
 		}
 	}
