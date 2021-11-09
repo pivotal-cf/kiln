785fe93bd03d48ea4b5c89ae3b2fa26450c1c7a7
Christopher Hunter <hunterch@vmware.com>
2021-11-08 19:12:37 -0800

    fix wip github release source and other stuff after refactoring types
    Authored-by: Christopher Hunter <hunterch@vmware.com>

--- internal/component/github_release_source_test.go
+++ internal/component/github_release_source_test.go
@@ -2,32 +2,46 @@ package component_test
 
 import (
 	"context"
-	"os"
 	"testing"
+
+	Ω "github.com/onsi/gomega"
 
 	"github.com/google/go-github/v40/github"
 	"github.com/pivotal-cf/kiln/internal/component"
 	"github.com/pivotal-cf/kiln/internal/component/fakes"
-	"github.com/pivotal-cf/kiln/pkg/cargo"
 )
 
 func TestListAllOfTheCrap(t *testing.T) {
-	grs := component.NewGithubReleaseSource(cargo.ReleaseSourceConfig{Type: component.ReleaseSourceTypeGithub, GithubToken: os.Getenv("GITHUB_TOKEN")})
-	grs.ListAllOfTheCrap(context.TODO(), "cloudfoundry")
+	t.SkipNow()
+
+	//grs := component.NewGithubReleaseSource(cargo.ReleaseSourceConfig{
+	//	Type: component.ReleaseSourceTypeGithub,
+	//	GithubToken: os.Getenv("GITHUB_TOKEN"),
+	//})
+	// grs.ListAllOfTheCrap(context.TODO(), "cloudfoundry")
+
+	// grs.Client.Repositories.GetReleaseByTag()
 }
 
-func TestGithubReleaseSource_GetMatchedRelease(t *testing.T) {
+func TestGithubReleaseSource_ComponentLockFromGithubRelease(t *testing.T) {
+	damnit := Ω.NewWithT(t)
 	strPtr := func(s string) *string { return &s }
-	lister := new(fakes.GithubReleaseLister)
-	lister.ListReleasesReturns([]*github.RepositoryRelease{
-		{
+
+	t.Run("release is found", func(t *testing.T) {
+		tagger := new(fakes.GetReleaseByTagger)
+
+		tagger.GetReleaseByTagReturns(&github.RepositoryRelease{
 			TagName: strPtr("0.226.0"),
-		},
-	}, nil, nil)
-	grs := component.GithubReleaseSource{}
-	_, found, err := grs.GetMatchedReleaseImpl(lister, component.Requirement{Version: "0.226.0"})
-	if !found {
-		panic("Error TestGithubReleaseSource_GetMatchedRelease: " + err.Error())
-	}
-	//Returns the Lock, but not sure what we want to do with it to determine success/failure
+		}, nil, nil)
+
+		ctx := context.TODO()
+
+		lock, _, _ := component.LockFromGithubRelease(ctx, tagger, component.Spec{
+			Name:    "routing",
+			Version: "0.226.0",
+		})
+
+		damnit.Expect(lock.Version).To(Ω.Equal("0.226.0"))
+		// damnit.Expect(lock.Name).To(Ω.Equal("routing"))
+	})
 }
