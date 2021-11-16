f0270d1a4397ece46cdcf25b43b3799190f7667a
Christopher Hunter <hunterch@vmware.com>
2021-11-16 14:53:54 -0800

    go fmt
    Authored-by: Christopher Hunter <hunterch@vmware.com>

--- internal/component/s3_release_source.go
+++ internal/component/s3_release_source.go
@@ -24,7 +24,6 @@ import (
 	"github.com/pivotal-cf/kiln/pkg/cargo"
 )
 
-
 //counterfeiter:generate -o ./fakes/s3_downloader.go --fake-name S3Downloader . S3Downloader
 type S3Downloader interface {
 	Download(w io.WriterAt, input *s3.GetObjectInput, options ...func(*s3manager.Downloader)) (n int64, err error)
