e6ea5810ccbd6b0b01d4bf8fa9a34645212c9276
Christopher Hunter <hunterch@vmware.com>
2021-11-16 15:53:28 -0800

    refactor: add test coverage for getting remote repo

--- internal/commands/release_notes_test.go
+++ internal/commands/release_notes_test.go
@@ -5,12 +5,13 @@ import (
 	"io/ioutil"
 	"testing"
 
+	Ω "github.com/onsi/gomega"
+	"github.com/pivotal-cf/jhanda"
+
 	"github.com/go-git/go-billy/v5/memfs"
 	"github.com/go-git/go-git/v5"
 	"github.com/go-git/go-git/v5/plumbing"
 	"github.com/go-git/go-git/v5/storage/memory"
-	Ω "github.com/onsi/gomega"
-	"github.com/pivotal-cf/jhanda"
 
 	"github.com/pivotal-cf/kiln/internal/commands"
 	"github.com/pivotal-cf/kiln/internal/commands/fakes"
@@ -30,124 +31,71 @@ func TestReleaseNotes_Usage(t *testing.T) {
 }
 
 func TestReleaseNotes_Execute(t *testing.T) {
-	t.Run("it generates complete release notes", func(t *testing.T) {
-		please := Ω.NewWithT(t)
+	please := Ω.NewWithT(t)
 
-		repo, _ := git.Init(memory.NewStorage(), memfs.New())
+	repo, _ := git.Init(memory.NewStorage(), memfs.New())
 
-		revisionResolver := new(fakes.RevisionResolver)
-		var (
-			initialHash, finalHash plumbing.Hash
-		)
-		fill(initialHash[:], '1')
-		fill(finalHash[:], '9')
-		revisionResolver.ResolveRevisionReturnsOnCall(0, &initialHash, nil)
-		revisionResolver.ResolveRevisionReturnsOnCall(1, &finalHash, nil)
+	revisionResolver := new(fakes.RevisionResolver)
+	var (
+		initialHash, finalHash plumbing.Hash
+	)
+	fill(initialHash[:], '1')
+	fill(finalHash[:], '9')
+	revisionResolver.ResolveRevisionReturnsOnCall(0, &initialHash, nil)
+	revisionResolver.ResolveRevisionReturnsOnCall(1, &finalHash, nil)
 
-		historicKilnfileLock := new(fakes.HistoricKilnfileLock)
-		historicKilnfileLock.ReturnsOnCall(0, cargo.KilnfileLock{
-			Releases: []cargo.ComponentLock{
-				{Name: "banana", Version: "0.1.0"},
-				{Name: "lemon", Version: "1.1.0"},
-			},
-		}, nil)
-		historicKilnfileLock.ReturnsOnCall(1, cargo.KilnfileLock{
-			Releases: []cargo.ComponentLock{
-				{Name: "banana", Version: "0.1.0"},
-				{Name: "lemon", Version: "1.2.0"},
-			},
-		}, nil)
+	historicKilnfileLock := new(fakes.HistoricKilnfileLock)
+	historicKilnfileLock.ReturnsOnCall(0, cargo.KilnfileLock{
+		Releases: []cargo.ComponentLock{
+			{Name: "banana", Version: "0.1.0"},
+			{Name: "lemon", Version: "1.1.0"},
+		},
+	}, nil)
+	historicKilnfileLock.ReturnsOnCall(1, cargo.KilnfileLock{
+		Releases: []cargo.ComponentLock{
+			{Name: "banana", Version: "0.1.0"},
+			{Name: "lemon", Version: "1.2.0"},
+		},
+	}, nil)
 
-		historicVersion := new(fakes.HistoricVersion)
-		historicVersion.Returns("0.1.0-build.50000", nil)
+	historicVersion := new(fakes.HistoricVersion)
+	historicVersion.Returns("0.1.0-build.50000", nil)
 
-		readFileCount := 0
-		readFileFunc := func(fp string) ([]byte, error) {
-			readFileCount++
-			return nil, nil
-		}
+	readFileCount := 0
+	readFileFunc := func(fp string) ([]byte, error) {
+		readFileCount++
+		return nil, nil
+	}
 
-		var output bytes.Buffer
-		rn := commands.ReleaseNotes{
-			Repository:           repo,
-			RevisionResolver:     revisionResolver,
-			HistoricKilnfileLock: historicKilnfileLock.Spy,
-			HistoricVersion:      historicVersion.Spy,
-			Writer:               &output,
-			ReadFile:             readFileFunc,
-		}
+	var output bytes.Buffer
+	rn := commands.ReleaseNotes{
+		Repository:           repo,
+		RevisionResolver:     revisionResolver,
+		HistoricKilnfileLock: historicKilnfileLock.Spy,
+		HistoricVersion:      historicVersion.Spy,
+		Writer:               &output,
+		ReadFile:             readFileFunc,
+	}
 
-		err := rn.Execute([]string{
-			"--release-date=2021-11-05",
-			"tile/1.1.0",
-			"tile/1.2.0",
-		})
-		please.Expect(err).NotTo(Ω.HaveOccurred())
-
-		please.Expect(revisionResolver.ResolveRevisionCallCount()).To(Ω.Equal(2))
-		please.Expect(revisionResolver.ResolveRevisionArgsForCall(0)).To(Ω.Equal(plumbing.Revision("tile/1.1.0")))
-		please.Expect(revisionResolver.ResolveRevisionArgsForCall(1)).To(Ω.Equal(plumbing.Revision("tile/1.2.0")))
-
-		please.Expect(historicVersion.CallCount()).To(Ω.Equal(1))
-		_, historicVersionHashArg, _ := historicVersion.ArgsForCall(0)
-		please.Expect(historicVersionHashArg).To(Ω.Equal(finalHash))
-
-		please.Expect(readFileCount).To(Ω.Equal(0))
-		expected, err := ioutil.ReadFile("testdata/release_notes_output.md")
-		please.Expect(err).NotTo(Ω.HaveOccurred())
-		please.Expect(output.String()).To(Ω.Equal(string(expected)))
+	err := rn.Execute([]string{
+		"--release-date=2021-11-05",
+		"tile/1.1.0",
+		"tile/1.2.0",
 	})
+	please.Expect(err).NotTo(Ω.HaveOccurred())
 
-	t.Run("release-date", func(t *testing.T) {
-		t.Run("when the flag value is invalid", func(t *testing.T) {
-			repo, _ := git.Init(memory.NewStorage(), memfs.New())
-			revisionResolver := new(fakes.RevisionResolver)
-			revisionResolver.ResolveRevisionReturns(&plumbing.ZeroHash, nil)
-			historicKilnfileLockFunc := new(fakes.HistoricKilnfileLock)
-			historicKilnfileLockFunc.Returns(cargo.KilnfileLock{}, nil)
+	please.Expect(revisionResolver.ResolveRevisionCallCount()).To(Ω.Equal(2))
+	please.Expect(revisionResolver.ResolveRevisionArgsForCall(0)).To(Ω.Equal(plumbing.Revision("tile/1.1.0")))
+	please.Expect(revisionResolver.ResolveRevisionArgsForCall(1)).To(Ω.Equal(plumbing.Revision("tile/1.2.0")))
 
-			err := commands.ReleaseNotes{
-				Repository:           repo,
-				RevisionResolver:     revisionResolver,
-				HistoricKilnfileLock: historicKilnfileLockFunc.Spy,
-				Writer:               &bytes.Buffer{},
-				ReadFile:             func(fp string) (_ []byte, _ error) { return },
-			}.Execute([]string{`--release-date="Nov, 2020"`, "ref1", "ref2"})
+	please.Expect(historicVersion.CallCount()).To(Ω.Equal(1))
+	_, historicVersionHashArg, _ := historicVersion.ArgsForCall(0)
+	please.Expect(historicVersionHashArg).To(Ω.Equal(finalHash))
 
-			t.Run("it returns an error", func(t *testing.T) {
-				please := Ω.NewWithT(t)
-				please.Expect(err).To(Ω.MatchError(Ω.And(
-					Ω.ContainSubstring("release date could not be parsed:"),
-					Ω.ContainSubstring("cannot parse"),
-				)))
-			})
-		})
-	})
-
-	//t.Run("issues", func(t *testing.T) {
-	//	t.Run("token is not set", func(t *testing.T) {
-	//		please := Ω.NewWithT(t)
-	//
-	//		repo, _ := git.Init(memory.NewStorage(), memfs.New())
-	//		revisionResolver := new(fakes.RevisionResolver)
-	//		revisionResolver.ResolveRevisionReturns(&plumbing.ZeroHash, nil)
-	//		historicKilnfileLockFunc := new(fakes.HistoricKilnfileLock)
-	//		historicKilnfileLockFunc.Returns(cargo.KilnfileLock{}, nil)
-	//
-	//		err := commands.ReleaseNotes{
-	//			Repository:           repo,
-	//			RevisionResolver:     revisionResolver,
-	//			HistoricKilnfileLock: historicKilnfileLockFunc.Spy,
-	//			Writer:               &bytes.Buffer{},
-	//			ReadFile:             func(fp string) (_ []byte, _ error) { return },
-	//		}.Execute([]string{"--date=2021-11-05", "ref1", "ref2"})
-	//
-	//		please.Expect(err).To(Ω.MatchError(Ω.And(
-	//			Ω.ContainSubstring("release date could not be parsed:"),
-	//			Ω.ContainSubstring("cannot parse"),
-	//		)))
-	//	})
-	//})
+	please.Expect(readFileCount).To(Ω.Equal(0))
+	expected, err := ioutil.ReadFile("testdata/release_notes_output.md")
+	please.Expect(err).NotTo(Ω.HaveOccurred())
+	please.Expect(output.String()).To(Ω.Equal(string(expected)))
 }
 
 func fill(buf []byte, value byte) {
