5414ec661fc22124e366db8e950c4f88fab7bfa0
Christopher Hunter <hunterch@vmware.com>
2021-11-16 17:43:43 -0800

    refactor: add test coverage for internal release notes helpers
    - use separate fakes package to avoid circular dependency

--- internal/commands/release_notes_internal_test.go
+++ internal/commands/release_notes_internal_test.go
@@ -1,13 +1,20 @@
 package commands
 
 import (
+	"context"
+	"errors"
+	Ω "github.com/onsi/gomega"
+	"net/http"
+	"testing"
+
 	"github.com/go-git/go-billy/v5/memfs"
 	"github.com/go-git/go-git/v5"
 	"github.com/go-git/go-git/v5/config"
 	"github.com/go-git/go-git/v5/storage/memory"
-	Ω "github.com/onsi/gomega"
+	"github.com/google/go-github/v40/github"
+
+	fakes "github.com/pivotal-cf/kiln/internal/commands/fakes_internal"
 	"github.com/pivotal-cf/kiln/internal/component"
-	"testing"
 )
 
 func TestInternal_ReleaseNotes_checkInputs(t *testing.T) {
@@ -236,3 +243,144 @@ func TestInternal_calculateComponentBumps(t *testing.T) {
 		)
 	})
 }
+
+func TestInternal_issuesFromIssueIDs(t *testing.T) {
+	t.Parallel()
+
+	t.Run("no ids", func(t *testing.T) {
+		please := Ω.NewWithT(t)
+		issuesService := new(fakes.IssueGetter)
+
+		result, err := issuesFromIssueIDs(context.TODO(), issuesService, "o", "n", nil)
+		please.Expect(err).NotTo(Ω.HaveOccurred())
+		please.Expect(result).To(Ω.HaveLen(0))
+		please.Expect(issuesService.GetCallCount()).To(Ω.Equal(0))
+	})
+
+	t.Run("some ids", func(t *testing.T) {
+		please := Ω.NewWithT(t)
+		issuesService := new(fakes.IssueGetter)
+
+		issuesService.GetReturnsOnCall(0, &github.Issue{Number: intPtr(1)}, githubResponse(t, 200), nil)
+		issuesService.GetReturnsOnCall(1, &github.Issue{Number: intPtr(2)}, githubResponse(t, 200), nil)
+
+		result, err := issuesFromIssueIDs(context.TODO(), issuesService, "o", "n", []string{"1", "2"})
+		please.Expect(err).NotTo(Ω.HaveOccurred())
+
+		please.Expect(result).To(Ω.HaveLen(2))
+		please.Expect(result[0].GetNumber()).To(Ω.Equal(1))
+		please.Expect(result[1].GetNumber()).To(Ω.Equal(2))
+
+		please.Expect(issuesService.GetCallCount()).To(Ω.Equal(2))
+		ctx, ro, rn, number := issuesService.GetArgsForCall(0)
+		please.Expect(ctx).NotTo(Ω.BeNil())
+		please.Expect(ro).To(Ω.Equal("o"))
+		please.Expect(rn).To(Ω.Equal("n"))
+		please.Expect(number).To(Ω.Equal(1))
+
+		_, _, _, number = issuesService.GetArgsForCall(1)
+		please.Expect(number).To(Ω.Equal(2))
+	})
+
+	t.Run("the issues service returns an error", func(t *testing.T) {
+		please := Ω.NewWithT(t)
+		issuesService := new(fakes.IssueGetter)
+
+		issuesService.GetReturnsOnCall(0, &github.Issue{Number: intPtr(1)}, nil, errors.New("banana"))
+
+		_, err := issuesFromIssueIDs(context.TODO(), issuesService, "o", "n", []string{"1"})
+		please.Expect(err).To(Ω.HaveOccurred())
+	})
+
+	t.Run("the issues service returns a not okay status", func(t *testing.T) {
+		please := Ω.NewWithT(t)
+		issuesService := new(fakes.IssueGetter)
+
+		issuesService.GetReturnsOnCall(0, &github.Issue{Number: intPtr(1)}, githubResponse(t, http.StatusUnauthorized), nil)
+
+		_, err := issuesFromIssueIDs(context.TODO(), issuesService, "o", "n", []string{"1"})
+		please.Expect(err).To(Ω.HaveOccurred())
+	})
+}
+
+func TestInternal_resolveMilestoneNumber(t *testing.T) {
+	t.Parallel()
+
+	t.Run("empty milestone option", func(t *testing.T) {
+		please := Ω.NewWithT(t)
+		issuesService := new(fakes.MilestoneLister)
+
+		result, err := resolveMilestoneNumber(context.TODO(), issuesService, "o", "n", "")
+		please.Expect(err).NotTo(Ω.HaveOccurred())
+		please.Expect(result).To(Ω.Equal(""))
+	})
+
+	// TODO: test pagination
+
+	t.Run("the issues service returns an error", func(t *testing.T) {
+		please := Ω.NewWithT(t)
+		issuesService := new(fakes.MilestoneLister)
+
+		issuesService.ListMilestonesReturns(nil, nil, errors.New("banana"))
+
+		_, err := resolveMilestoneNumber(context.TODO(), issuesService, "o", "n", "m")
+		please.Expect(err).To(Ω.HaveOccurred())
+	})
+
+	t.Run("the issues service returns a not okay status", func(t *testing.T) {
+		please := Ω.NewWithT(t)
+		issuesService := new(fakes.MilestoneLister)
+
+		issuesService.ListMilestonesReturns(nil, githubResponse(t, http.StatusUnauthorized), nil)
+
+		_, err := resolveMilestoneNumber(context.TODO(), issuesService, "o", "n", "m")
+		please.Expect(err).To(Ω.HaveOccurred())
+	})
+}
+func TestInternal_fetchIssuesWithLabelAndMilestone(t *testing.T) {
+	t.Parallel()
+
+	t.Run("empty milestone and labels", func(t *testing.T) {
+		please := Ω.NewWithT(t)
+		issuesService := new(fakes.IssuesByRepoLister)
+
+		result, err := fetchIssuesWithLabelAndMilestone(context.TODO(), issuesService, "o", "n", "", nil)
+		please.Expect(err).NotTo(Ω.HaveOccurred())
+		please.Expect(result).To(Ω.HaveLen(0))
+	})
+
+	// TODO: issue service call params
+
+	t.Run("the issues service returns an error", func(t *testing.T) {
+		please := Ω.NewWithT(t)
+		issuesService := new(fakes.IssuesByRepoLister)
+
+		issuesService.ListByRepoReturns(nil, nil, errors.New("banana"))
+
+		_, err := fetchIssuesWithLabelAndMilestone(context.TODO(), issuesService, "o", "n", "1", nil)
+		please.Expect(err).To(Ω.HaveOccurred())
+	})
+
+	t.Run("the issues service returns a not okay status", func(t *testing.T) {
+		please := Ω.NewWithT(t)
+		issuesService := new(fakes.IssuesByRepoLister)
+
+		issuesService.ListByRepoReturns(nil, githubResponse(t, http.StatusUnauthorized), nil)
+
+		_, err := fetchIssuesWithLabelAndMilestone(context.TODO(), issuesService, "o", "n", "1", nil)
+		please.Expect(err).To(Ω.HaveOccurred())
+	})
+}
+
+func githubResponse(t *testing.T, status int) *github.Response {
+	t.Helper()
+
+	return &github.Response{
+		Response: &http.Response{
+			StatusCode: status,
+			Status:     http.StatusText(status),
+		},
+	}
+}
+
+func intPtr(n int) *int { return &n }
