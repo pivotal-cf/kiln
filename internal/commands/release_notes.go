9afc0762775ba9125cb875351d15a9e7847c3052
Christopher Hunter <hunterch@vmware.com>
2021-11-19 16:41:57 -0800

    wip: add release-notes command
    Authored-by: Christopher Hunter <hunterch@vmware.com>

---
+++ internal/commands/release_notes.go
@@ -0,0 +1,175 @@
+package commands
+
+import (
+	_ "embed"
+	"io"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"text/template"
+	"time"
+
+	"github.com/go-git/go-git/v5"
+	"github.com/go-git/go-git/v5/plumbing"
+	"github.com/pivotal-cf/jhanda"
+
+	"github.com/pivotal-cf/kiln/internal/component"
+	"github.com/pivotal-cf/kiln/internal/historic"
+	"github.com/pivotal-cf/kiln/pkg/cargo"
+)
+
+const releaseDateFormat = "01/02/2006"
+
+type ReleaseNotes struct {
+	Options struct {
+		Version      string `short:"v" long:"version" description:"version of the tile"`    // TODO version should come from final revision not flag
+		ReleaseDate  string `short:"rd" long:"date" description:"release date of the tile"` // TODO version should come from final revision not flag
+		TemplateName string `short:"t" long:"template" description:"path to template"`
+	}
+
+	pathRelativeToDotGit string
+	Repository           *git.Repository
+	ReadFile             func(fp string) ([]byte, error)
+	KilnfileLockAtCommit HistoricKilnfileLockFunc
+	RevisionResolver
+	Stat func(string) (os.FileInfo, error)
+	Now  func() time.Time
+	io.Writer
+}
+
+func NewReleaseNotesCommand() (ReleaseNotes, error) {
+	repo, err := git.PlainOpenWithOptions(".", &git.PlainOpenOptions{DetectDotGit: true})
+	if err != nil {
+		return ReleaseNotes{}, err
+	}
+	wt, err := repo.Worktree()
+	if err != nil {
+		return ReleaseNotes{}, err
+	}
+	wd, err := os.Getwd()
+	if err != nil {
+		return ReleaseNotes{}, err
+	}
+	rp, err := filepath.Rel(wt.Filesystem.Root(), wd)
+	if err != nil {
+		return ReleaseNotes{}, err
+	}
+	return ReleaseNotes{
+		Repository:           repo,
+		ReadFile:             ioutil.ReadFile,
+		KilnfileLockAtCommit: historic.KilnfileLock,
+		RevisionResolver:     repo,
+		Stat:                 os.Stat,
+		Writer:               os.Stdout,
+		Now:                  time.Now,
+		pathRelativeToDotGit: rp,
+	}, nil
+}
+
+//counterfeiter:generate -o ./fakes/historic_kilnfile_lock_func.go --fake-name HistoricKilnfileLockFunc . HistoricKilnfileLockFunc
+
+type HistoricKilnfileLockFunc func(repo *git.Repository, commitHash plumbing.Hash, kilnfilePath string) (cargo.KilnfileLock, error)
+
+//counterfeiter:generate -o ./fakes/revision_resolver.go --fake-name RevisionResolver . RevisionResolver
+
+type RevisionResolver interface {
+	ResolveRevision(rev plumbing.Revision) (*plumbing.Hash, error)
+}
+
+func (r ReleaseNotes) Execute(args []string) error {
+	nonFlagArgs, err := jhanda.Parse(&r.Options, args) // TODO handle error
+	if err != nil {
+		return err
+	}
+
+	// TODO ensure len(nonFlagArgs) < 2
+
+	releaseDate, _ := r.releaseDate()
+
+	initialCommitSHA, err := r.ResolveRevision(plumbing.Revision(nonFlagArgs[0])) // TODO handle error
+	if err != nil {
+		panic(err)
+	}
+	finalCommitSHA, err := r.ResolveRevision(plumbing.Revision(nonFlagArgs[1])) // TODO handle error
+	if err != nil {
+		panic(err)
+	}
+
+	klInitial, err := r.KilnfileLockAtCommit(r.Repository, *initialCommitSHA, r.pathRelativeToDotGit) // TODO handle error
+	if err != nil {
+		panic(err)
+	}
+	klFinal, err := r.KilnfileLockAtCommit(r.Repository, *finalCommitSHA, r.pathRelativeToDotGit) // TODO handle error
+	if err != nil {
+		panic(err)
+	}
+
+	info := ReleaseNotesInformation{
+		Version:           r.Options.Version, // TODO version should come from version file at final revision and then maybe override with flag
+		ReleaseDate:       releaseDate,
+		ReleaseDateFormat: releaseDateFormat,
+		// Issues:      issues,
+		Components: klFinal.Releases,
+		Bumps:      calculateReleaseBumps(klFinal.Releases, klInitial.Releases),
+	}
+
+	releaseNotesTemplate := defaultReleaseNotesTemplate
+	if r.Options.TemplateName != "" {
+		templateBuf, _ := r.ReadFile(r.Options.TemplateName) // TODO handle error
+		releaseNotesTemplate = string(templateBuf)
+	}
+
+	t, err := template.New(r.Options.TemplateName).Parse(releaseNotesTemplate) // TODO handle error
+	if err != nil {
+		panic(err)
+	}
+
+	return t.Execute(r.Writer, info)
+}
+
+func (r ReleaseNotes) Usage() jhanda.Usage {
+	return jhanda.Usage{
+		Description:      "generates release notes from bosh-release release notes on GitHub between two tile repo git references",
+		ShortDescription: "generates release notes from bosh-release release notes",
+		Flags:            r.Options,
+	}
+}
+
+func (r ReleaseNotes) releaseDate() (time.Time, error) {
+	if r.Options.ReleaseDate == "" {
+		return r.Now(), nil
+	}
+	return time.Parse(releaseDateFormat, r.Options.ReleaseDate) // TODO handle error
+}
+
+//go:embed release_notes.md.template
+var defaultReleaseNotesTemplate string
+
+type ReleaseNotesInformation struct {
+	Version           string
+	ReleaseDate       time.Time
+	ReleaseDateFormat string
+	// Issues      []*github.Issue
+	Bumps      []component.Lock
+	Components []component.Lock
+}
+
+type BoshReleaseBump = component.Spec
+
+func calculateReleaseBumps(current, previous []component.Lock) []component.Lock {
+	var (
+		bumps         []component.Lock
+		previousSpecs = make(map[component.Lock]struct{}, len(previous))
+	)
+	for _, cs := range previous {
+		previousSpecs[cs] = struct{}{}
+	}
+	for _, cs := range current {
+		_, isSame := previousSpecs[cs]
+		if isSame {
+			continue
+		}
+		bumps = append(bumps, cs)
+	}
+	return bumps
+}
---
+++ internal/historic/historic_test.go
@@ -0,0 +1,277 @@
+package historic
+
+import (
+	"testing"
+	"time"
+
+	Ω "github.com/onsi/gomega"
+
+	"github.com/go-git/go-billy/v5/memfs"
+	"github.com/go-git/go-git/v5"
+	"github.com/go-git/go-git/v5/plumbing"
+	"github.com/go-git/go-git/v5/plumbing/object"
+	"github.com/go-git/go-git/v5/storage/memory"
+
+	"github.com/pivotal-cf/kiln/pkg/cargo"
+)
+
+func TestVersion(t *testing.T) {
+	please := Ω.NewWithT(t)
+
+	// START setup
+	tileDir := "tile"
+	repo, _ := git.Init(memory.NewStorage(), memfs.New())
+	initialHash := commit(t, repo, "alpha release", func(wt *git.Worktree) error {
+		p := wt.Filesystem.Join(tileDir, "version")
+		kf, _ := wt.Filesystem.Create(p)
+		_, _ = kf.Write([]byte("1.0.0-alpha.1\n"))
+		_ = kf.Close()
+		_, _ = wt.Add(p)
+		return nil
+	})
+	change := commit(t, repo, "some other change", func(wt *git.Worktree) error {
+		p := wt.Filesystem.Join(tileDir, "base.yml")
+		kf, _ := wt.Filesystem.Create(p)
+		_, _ = kf.Write([]byte("---\nname: something\n"))
+		_ = kf.Close()
+		_, _ = wt.Add(p)
+		return nil
+	}, initialHash)
+	finalHash := commit(t, repo, "ga release", func(wt *git.Worktree) error {
+		p := wt.Filesystem.Join(tileDir, "version")
+		kf, _ := wt.Filesystem.Create(p)
+		_, _ = kf.Write([]byte("1.0.0\n"))
+		_ = kf.Close()
+		_, _ = wt.Add(p)
+		return nil
+	}, change)
+	// END setup
+
+	t.Run("alpha", func(t *testing.T) {
+		version, err := Version(repo, initialHash, "tile")
+
+		please.Expect(err).NotTo(Ω.HaveOccurred())
+		please.Expect(version).To(Ω.Equal("1.0.0-alpha.1"))
+	})
+
+	t.Run("ga release", func(t *testing.T) {
+		version, err := Version(repo, finalHash, "tile")
+
+		please.Expect(err).NotTo(Ω.HaveOccurred())
+		please.Expect(version).To(Ω.Equal("1.0.0"))
+	})
+}
+
+func TestKilnfileLock(t *testing.T) {
+	please := Ω.NewWithT(t)
+
+	// START setup
+	tileDir := "tile"
+	repo, _ := git.Init(memory.NewStorage(), memfs.New())
+	initialHash := commit(t, repo, "initial", func(wt *git.Worktree) error {
+		p := wt.Filesystem.Join(tileDir, "assets.lock")
+		kf, _ := wt.Filesystem.Create(p)
+		_, _ = kf.Write([]byte(initialKilnfileLock))
+		_ = kf.Close()
+		_, _ = wt.Add(p)
+		return nil
+	})
+	middleHash := commit(t, repo, "some other change", func(wt *git.Worktree) error {
+		p := wt.Filesystem.Join(tileDir, "base.yml")
+		kf, _ := wt.Filesystem.Create(p)
+		_, _ = kf.Write([]byte("---\nname: something\n"))
+		_ = kf.Close()
+		_, _ = wt.Add(p)
+		return nil
+	}, initialHash)
+	badYAML := commit(t, repo, "add some non-yaml", func(wt *git.Worktree) error {
+		p := wt.Filesystem.Join(tileDir, "Kilnfile.lock")
+		kf, _ := wt.Filesystem.Create(p)
+		_, _ = kf.Write([]byte(`{{ if eq tile "ert"}}# this is ERT{{}}\n` + finalKilnfileLock))
+		_ = kf.Close()
+		_, _ = wt.Add(p)
+		return nil
+	}, middleHash)
+	finalHash := commit(t, repo, "fix bad yaml", func(wt *git.Worktree) error {
+		p := wt.Filesystem.Join(tileDir, "Kilnfile.lock")
+		kf, _ := wt.Filesystem.Create(p)
+		_, _ = kf.Write([]byte(finalKilnfileLock))
+		_ = kf.Close()
+		_, _ = wt.Add(p)
+		return nil
+	}, badYAML)
+	// END setup
+
+	t.Run("legacy bill of materials", func(t *testing.T) {
+		kf, err := KilnfileLock(repo, initialHash, "tile")
+
+		please.Expect(err).NotTo(Ω.HaveOccurred())
+		please.Expect(kf.Releases).To(Ω.Equal([]cargo.ComponentLock{
+			{Name: "banana", Version: "0.1.0"},
+			{Name: "lemon", Version: "1.1.0"},
+		}))
+	})
+
+	t.Run("kilnfile.lock", func(t *testing.T) {
+		finalKF, err := KilnfileLock(repo, finalHash, "tile/Kilnfile")
+
+		please.Expect(err).NotTo(Ω.HaveOccurred())
+		please.Expect(finalKF.Releases).To(Ω.Equal([]cargo.ComponentLock{
+			{Name: "banana", Version: "0.9.0"},
+			{Name: "lemon", Version: "1.9.0"},
+			{Name: "apple", Version: "0.0.1"},
+		}))
+	})
+
+	t.Run("bad yaml", func(t *testing.T) {
+		_, err := KilnfileLock(repo, badYAML, "tile")
+
+		please.Expect(err).To(Ω.MatchError(Ω.ContainSubstring("cannot unmarshal")))
+	})
+}
+
+const (
+	initialKilnfileLock = `---
+releases:
+- name: banana
+  version: 0.1.0
+- name: lemon
+  version: 1.1.0
+`
+
+	finalKilnfileLock = `---
+releases:
+- name: banana
+  version: 0.9.0
+- name: lemon
+  version: 1.9.0
+- name: apple
+  version: 0.0.1
+`
+)
+
+func TestWalk(t *testing.T) {
+	t.Run("simple walk", func(t *testing.T) {
+		// START setup
+		tileDir := "tile"
+		repo, _ := git.Init(memory.NewStorage(), memfs.New())
+
+		h0 := commit(t, repo, "alpha release", func(wt *git.Worktree) error {
+			p := wt.Filesystem.Join(tileDir, "version")
+			kf, _ := wt.Filesystem.Create(p)
+			_, _ = kf.Write([]byte("1.0.0-alpha.1\n"))
+			_ = kf.Close()
+			_, _ = wt.Add(p)
+			return nil
+		})
+		h1 := commit(t, repo, "some other change", func(wt *git.Worktree) error {
+			p := wt.Filesystem.Join(tileDir, "base.yml")
+			kf, _ := wt.Filesystem.Create(p)
+			_, _ = kf.Write([]byte("---\nname: something\n"))
+			_ = kf.Close()
+			_, _ = wt.Add(p)
+			return nil
+		}, h0)
+		hf := commit(t, repo, "ga release", func(wt *git.Worktree) error {
+			p := wt.Filesystem.Join(tileDir, "version")
+			kf, _ := wt.Filesystem.Create(p)
+			_, _ = kf.Write([]byte("1.0.0\n"))
+			_ = kf.Close()
+			_, _ = wt.Add(p)
+			return nil
+		}, h1)
+		// END setup
+
+		please := Ω.NewWithT(t)
+
+		callCount := 0
+		err := Walk(repo, hf, func(*object.Commit) {
+			callCount++
+		})
+		please.Expect(err).NotTo(Ω.HaveOccurred())
+		please.Expect(callCount).To(Ω.Equal(3))
+	})
+
+	t.Run("with branch", func(t *testing.T) {
+		// START setup
+		tileDir := "tile"
+		repo, _ := git.Init(memory.NewStorage(), memfs.New())
+
+		h0 := commit(t, repo, "alpha release", func(wt *git.Worktree) error {
+			p := wt.Filesystem.Join(tileDir, "version")
+			kf, _ := wt.Filesystem.Create(p)
+			_, _ = kf.Write([]byte("1.0.0-alpha.1\n"))
+			_ = kf.Close()
+			_, _ = wt.Add(p)
+			return nil
+		})
+		h1 := commit(t, repo, "some other change", func(wt *git.Worktree) error {
+			p := wt.Filesystem.Join(tileDir, "base.yml")
+			kf, _ := wt.Filesystem.Create(p)
+			_, _ = kf.Write([]byte("---\nname: something-else\n"))
+			_ = kf.Close()
+			_, _ = wt.Add(p)
+			return nil
+		}, h0)
+
+		b1 := commit(t, repo, "some other change", func(wt *git.Worktree) error {
+			p := wt.Filesystem.Join(tileDir, "base.yml")
+			kf, _ := wt.Filesystem.Create(p)
+			_, _ = kf.Write([]byte("---\nname: something\n"))
+			_ = kf.Close()
+			_, _ = wt.Add(p)
+			return nil
+		}, h0)
+		b2 := commit(t, repo, "some other change", func(wt *git.Worktree) error {
+			p := wt.Filesystem.Join(tileDir, "base.yml")
+			kf, _ := wt.Filesystem.Create(p)
+			_, _ = kf.Write([]byte("---\nname: something-else\n"))
+			_ = kf.Close()
+			_, _ = wt.Add(p)
+			return nil
+		}, b1)
+
+		hf := commit(t, repo, "ga release", func(wt *git.Worktree) error {
+			p := wt.Filesystem.Join(tileDir, "version")
+			kf, _ := wt.Filesystem.Create(p)
+			_, _ = kf.Write([]byte("1.0.0\n"))
+			_ = kf.Close()
+			_, _ = wt.Add(p)
+			return nil
+		}, h1, b2)
+		// END setup
+
+		please := Ω.NewWithT(t)
+
+		callCount := 0
+		err := Walk(repo, hf, func(*object.Commit) {
+			callCount++
+		})
+		please.Expect(err).NotTo(Ω.HaveOccurred())
+		please.Expect(callCount).To(Ω.Equal(5))
+	})
+}
+
+func commit(t *testing.T, repo *git.Repository, msg string, fn func(wt *git.Worktree) error, parents ...plumbing.Hash) plumbing.Hash {
+	t.Helper()
+	wt, err := repo.Worktree()
+	if err != nil {
+		t.Fatal(err)
+	}
+	err = fn(wt)
+	if err != nil {
+		t.Fatal(err)
+	}
+	signature := &object.Signature{
+		Name:  "releen",
+		Email: "releen@example.com",
+		When:  time.Unix(1635975074, 0),
+	}
+	h, err := wt.Commit(msg, &git.CommitOptions{
+		Author: signature, Committer: signature, Parents: parents,
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+	return h
+}
