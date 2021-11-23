8bf51fd3f0a21a8afbee18548ec3949af09aedd1
Christopher Hunter <hunterch@vmware.com>
2021-11-23 09:05:41 -0800

    refactor: move helper functions to an _internal file

--- internal/commands/release_notes.go
+++ internal/commands/release_notes.go
@@ -7,13 +7,9 @@ import (
 	"fmt"
 	"io"
 	"io/ioutil"
-	"net/http"
 	"os"
 	"path/filepath"
 	"regexp"
-	"sort"
-	"strconv"
-	"strings"
 	"text/template"
 	"time"
 
@@ -50,6 +46,8 @@ type ReleaseNotes struct {
 	RevisionResolver
 	Stat func(string) (os.FileInfo, error)
 	io.Writer
+
+	GitHubAPI func(ctx context.Context, token string) releaseNotesGithubAPIClient
 
 	RepoOwner, RepoName string
 }
@@ -88,7 +86,21 @@ func NewReleaseNotesCommand() (ReleaseNotes, error) {
 		pathRelativeToDotGit: rp,
 		RepoName:             repoName,
 		RepoOwner:            repoOwner,
+
+		GitHubAPI: func(ctx context.Context, token string) releaseNotesGithubAPIClient {
+			tokenSource := oauth2.StaticTokenSource(&oauth2.Token{AccessToken: token})
+			tokenClient := oauth2.NewClient(ctx, tokenSource)
+			return github.NewClient(tokenClient).Issues
+		},
 	}, nil
+}
+
+func (r ReleaseNotes) Usage() jhanda.Usage {
+	return jhanda.Usage{
+		Description:      "generates release notes from bosh-release release notes on GitHub between two tile repo git references",
+		ShortDescription: "generates release notes from bosh-release release notes",
+		Flags:            r.Options,
+	}
 }
 
 //counterfeiter:generate -o ./fakes/historic_version.go --fake-name HistoricVersion . HistoricVersion
@@ -182,20 +194,6 @@ func (r ReleaseNotes) Execute(args []string) error {
 	return nil
 }
 
-func (r ReleaseNotes) parseReleaseDate() (time.Time, error) {
-	var releaseDate time.Time
-
-	if r.Options.ReleaseDate != "" {
-		var err error
-		releaseDate, err = time.Parse(releaseDateFormat, r.Options.ReleaseDate)
-		if err != nil {
-			return time.Time{}, fmt.Errorf("release date could not be parsed: %w", err)
-		}
-	}
-
-	return releaseDate, nil
-}
-
 func (r ReleaseNotes) checkInputs(nonFlagArgs []string) error {
 	if len(nonFlagArgs) != 2 {
 		return errors.New("expected two arguments: <Git-Revision> <Git-Revision>")
@@ -223,30 +221,18 @@ func (r ReleaseNotes) checkInputs(nonFlagArgs []string) error {
 	return nil
 }
 
-func getGithubRemoteRepoOwnerAndName(repo *git.Repository) (string, string, error) {
-	var remoteURL string
-	remote, err := repo.Remote("origin")
-	if err != nil {
-		return "", "", err
-	}
-	config := remote.Config()
-	for _, u := range config.URLs {
-		if !strings.Contains(u, "github.com") {
-			continue
+func (r ReleaseNotes) parseReleaseDate() (time.Time, error) {
+	var releaseDate time.Time
+
+	if r.Options.ReleaseDate != "" {
+		var err error
+		releaseDate, err = time.Parse(releaseDateFormat, r.Options.ReleaseDate)
+		if err != nil {
+			return time.Time{}, fmt.Errorf("release date could not be parsed: %w", err)
 		}
-		remoteURL = u
-		break
-	}
-	if remoteURL == "" {
-		return "", "", fmt.Errorf("remote github URL not found for repo")
 	}
 
-	repoOwner, repoName := component.OwnerAndRepoFromGitHubURI(remoteURL)
-	if repoOwner == "" || repoName == "" {
-		return "", "", errors.New("could not determine owner and repo for tile")
-	}
-
-	return repoOwner, repoName, nil
+	return releaseDate, nil
 }
 
 type ReleaseNotesInformation struct {
@@ -260,24 +246,10 @@ type ReleaseNotesInformation struct {
 	Components []component.Lock
 }
 
-type BoshReleaseBump = component.Lock
-
-func calculateComponentBumps(current, previous []component.Lock) []BoshReleaseBump {
-	var (
-		bumps         []BoshReleaseBump
-		previousSpecs = make(map[component.Lock]struct{}, len(previous))
-	)
-	for _, cs := range previous {
-		previousSpecs[cs] = struct{}{}
-	}
-	for _, cs := range current {
-		_, isSame := previousSpecs[cs]
-		if isSame {
-			continue
-		}
-		bumps = append(bumps, cs)
-	}
-	return bumps
+type releaseNotesGithubAPIClient interface {
+	issueGetter
+	milestoneLister
+	issuesByRepoLister
 }
 
 // listGithubIssues is not tested. By not testing we are getting reduced abstraction and improved readability at the
@@ -292,190 +264,20 @@ func (r ReleaseNotes) listGithubIssues(ctx context.Context) ([]*github.Issue, er
 		return nil, nil
 	}
 
-	tokenSource := oauth2.StaticTokenSource(&oauth2.Token{AccessToken: r.Options.GithubToken})
-	tokenClient := oauth2.NewClient(ctx, tokenSource)
-	githubClient := github.NewClient(tokenClient)
+	issuesService := r.GitHubAPI(ctx, r.Options.GithubToken)
 
-	milestoneNumber, err := resolveMilestoneNumber(ctx, githubClient.Issues, r.RepoOwner, r.RepoName, r.Options.IssueMilestone)
+	milestoneNumber, err := resolveMilestoneNumber(ctx, issuesService, r.RepoOwner, r.RepoName, r.Options.IssueMilestone)
 	if err != nil {
 		return nil, err
 	}
-	issues, err := fetchIssuesWithLabelAndMilestone(ctx, githubClient.Issues, r.RepoOwner, r.RepoName, milestoneNumber, r.Options.IssueLabels)
+	issues, err := fetchIssuesWithLabelAndMilestone(ctx, issuesService, r.RepoOwner, r.RepoName, milestoneNumber, r.Options.IssueLabels)
 	if err != nil {
 		return nil, err
 	}
-	additionalIssues, err := issuesFromIssueIDs(ctx, githubClient.Issues, r.RepoOwner, r.RepoName, r.Options.IssueIDs)
+	additionalIssues, err := issuesFromIssueIDs(ctx, issuesService, r.RepoOwner, r.RepoName, r.Options.IssueIDs)
 	if err != nil {
 		return nil, err
 	}
 
 	return appendFilterAndSortIssues(issues, additionalIssues, r.Options.IssueTitleExp), nil
-}
-
-//counterfeiter:generate -o ./fakes_internal/issue_getter.go --fake-name IssueGetter . issueGetter
-
-type issueGetter interface {
-	Get(ctx context.Context, owner string, repo string, number int) (*github.Issue, *github.Response, error)
-}
-
-func issuesFromIssueIDs(ctx context.Context, issuesService issueGetter, repoOwner, repoName string, issueIDs []string) ([]*github.Issue, error) {
-	var issues []*github.Issue
-	for _, id := range issueIDs {
-		n, err := strconv.Atoi(id)
-		if err != nil {
-			return nil, fmt.Errorf("failed to parse issue id %q: %w", id, err)
-		}
-		issue, response, err := issuesService.Get(ctx, repoOwner, repoName, n)
-		if err != nil {
-			return nil, err
-		}
-		if response.StatusCode != http.StatusOK {
-			return nil, fmt.Errorf("failed to get issue %q: %w", id, err)
-		}
-		issues = append(issues, issue)
-	}
-	return issues, nil
-}
-
-//counterfeiter:generate -o ./fakes_internal/milestone_lister.go --fake-name MilestoneLister . milestoneLister
-
-type milestoneLister interface {
-	ListMilestones(ctx context.Context, owner string, repo string, opts *github.MilestoneListOptions) ([]*github.Milestone, *github.Response, error)
-}
-
-func resolveMilestoneNumber(ctx context.Context, issuesService milestoneLister, repoOwner, repoName, milestone string) (string, error) {
-	if milestone == "" {
-		return "", nil
-	}
-	_, parseErr := strconv.Atoi(milestone)
-	if parseErr == nil {
-		return milestone, nil
-	}
-
-	queryOptions := &github.MilestoneListOptions{}
-	for {
-		ms, res, err := issuesService.ListMilestones(ctx, repoOwner, repoName, queryOptions)
-		if err != nil {
-			return "", err
-		}
-		if res.Response.StatusCode != http.StatusOK {
-			return "", fmt.Errorf("unexpedted status code %d", res.Response.StatusCode)
-		}
-		for _, m := range ms {
-			if m.GetTitle() == milestone {
-				return strconv.Itoa(m.GetNumber()), nil
-			}
-		}
-		queryOptions.Page++
-	}
-}
-
-//counterfeiter:generate -o ./fakes_internal/issues_by_repo_lister.go --fake-name IssuesByRepoLister . issuesByRepoLister
-
-type issuesByRepoLister interface {
-	ListByRepo(ctx context.Context, owner string, repo string, opts *github.IssueListByRepoOptions) ([]*github.Issue, *github.Response, error)
-}
-
-func fetchIssuesWithLabelAndMilestone(ctx context.Context, issuesService issuesByRepoLister, repoOwner, repoName, milestoneNumber string, labels []string) ([]*github.Issue, error) {
-	if milestoneNumber == "" && len(labels) == 0 {
-		return nil, nil
-	}
-	// TODO: handle pagination
-	issueList, response, err := issuesService.ListByRepo(ctx, repoOwner, repoName, &github.IssueListByRepoOptions{
-		Milestone: milestoneNumber,
-		Labels:    labels,
-	})
-	if err != nil {
-		return nil, err
-	}
-	if response.StatusCode != http.StatusOK {
-		return nil, fmt.Errorf("failed to get issues: %w", err)
-	}
-	return issueList, nil
-}
-
-func appendFilterAndSortIssues(issuesA, issuesB []*github.Issue, filterExp string) []*github.Issue {
-	isIDEqual := func(a, b *github.Issue) bool {
-		return a.GetID() == b.GetID()
-	}
-	fullList := appendUnique(issuesA, isIDEqual, issuesB...)
-
-	filtered := filterIssuesByTitle(filterExp, fullList)
-
-	sort.Sort(IssuesBySemanticTitlePrefix(filtered))
-
-	return filtered
-}
-
-func appendUnique(list []*github.Issue, equal func(a, b *github.Issue) bool, additional ...*github.Issue) []*github.Issue {
-nextNewIssue:
-	for _, newIssue := range additional {
-		for _, existingIssue := range list {
-			if equal(newIssue, existingIssue) {
-				continue nextNewIssue
-			}
-		}
-		list = append(list, newIssue)
-	}
-	return list
-}
-
-func filterIssuesByTitle(exp string, issues []*github.Issue) []*github.Issue {
-	if exp == "" {
-		return issues
-	}
-	titleCheck := regexp.MustCompile(exp)
-	filtered := issues[:0]
-	for _, issue := range issues {
-		if !titleCheck.MatchString(issue.GetTitle()) {
-			continue
-		}
-		filtered = append(filtered, issue)
-	}
-	return filtered
-}
-
-// IssuesBySemanticTitlePrefix sorts issues by title lexicographically. It handles issues with a prefix like
-// \*\*\[(security fix)|(feature)|(feature improvement)|(bug fix)|(none)\]\*\*, differently and sorts them
-// in order of importance.
-type IssuesBySemanticTitlePrefix []*github.Issue
-
-func (list IssuesBySemanticTitlePrefix) Len() int { return len(list) }
-
-func (list IssuesBySemanticTitlePrefix) Swap(i, j int) { list[i], list[j] = list[j], list[i] }
-
-func (list IssuesBySemanticTitlePrefix) Less(i, j int) bool {
-	it := list[i].GetTitle()
-	jt := list[j].GetTitle()
-	iv := issuesTitlePrefixSemanticValue(it)
-	jv := issuesTitlePrefixSemanticValue(jt)
-	if iv != jv {
-		return iv > jv
-	}
-	return strings.Compare(it, jt) < 0
-}
-
-func issuesTitlePrefixSemanticValue(title string) int {
-	title = strings.ToLower(title)
-	prefixes := []string{
-		"**[security fix]**",
-		"**[feature]**",
-		"**[feature improvement]**",
-		"**[bug fix]**",
-		"**[none]**",
-	}
-	for i, v := range prefixes {
-		if strings.HasPrefix(title, v) {
-			return len(prefixes) - i
-		}
-	}
-	return 0
-}
-
-func (r ReleaseNotes) Usage() jhanda.Usage {
-	return jhanda.Usage{
-		Description:      "generates release notes from bosh-release release notes on GitHub between two tile repo git references",
-		ShortDescription: "generates release notes from bosh-release release notes",
-		Flags:            r.Options,
-	}
 }
