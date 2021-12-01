b521320f7a18bd17e23bad62eeb2eeb31d343f36
Ryan Hall <hallr@vmware.com>
2021-12-01 11:27:30 -0800

    fix: field order change causes lockfile conflicts
    - We inadvertently altered the field order in our ComponentLock struct
    - Fixed and added test to validate ordering
    
    Co-authored-by: Steve Taylor <sttaylor@vmware.com>
    Co-authored-by: Ryan Hall <hallr@vmware.com>
    Co-authored-by: Christopher Hunter <hunterch@vmware.com>

--- pkg/cargo/kilnfile.go
+++ pkg/cargo/kilnfile.go
@@ -32,7 +32,6 @@ type ComponentSpec struct {
 	//Repositories []string `yaml:"repositories,omitempty"`
 }
 
-
 // VersionConstraints must be passed a spec with a parsable
 // semver. The Kiln Validate command ensures that the versions
 // in this field continue to stay valid.
@@ -91,8 +90,8 @@ type ReleaseSourceConfig struct {
 // used as a key type in a map. Don't add array or map fields.
 type ComponentLock struct {
 	Name    string `yaml:"name"`
-	Version string `yaml:"version,omitempty"`
 	SHA1    string `yaml:"sha1"`
+	Version string `yaml:"version,omitempty"`
 
 	StemcellOS      string `yaml:"os,omitempty"`
 	StemcellVersion string `yaml:"stemcell_version,omitempty"`
