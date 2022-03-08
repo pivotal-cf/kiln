8548a84a4d96c7213113f3468697fa56997066a1
Christopher Hunter <8398225+crhntr@users.noreply.github.com>
2022-03-07 16:06:09 -0800

    refactor (update-boshio-index): use the same code as the release source

--- internal/component/update-boshio-index/main.go
+++ internal/component/update-boshio-index/main.go
@@ -1,12 +1,11 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"io"
-	"net/http"
-	"net/url"
+	"log"
 	"os"
-	"path"
 
 	"gopkg.in/yaml.v2"
 
@@ -40,42 +39,10 @@ func main() {
 		out = f
 	}
 
-	var indexRecords []component.BoshReleaseRepositoryRecord
-	err := getAndParse("https://raw.githubusercontent.com/bosh-io/releases/HEAD/index.yml", &indexRecords)
-	if err != nil {
-		panic(err)
+	indexRecords, errList := component.GetBoshReleaseRepositoryIndex(context.Background())
+	for _, err := range errList {
+		log.Println(err)
 	}
-
-	hydrateCache(cachedIndex, indexRecords)
-
-	for i, record := range indexRecords {
-		if record.URL == "" {
-			continue
-		}
-		if record.Name != "" {
-			_, _ = fmt.Fprintf(os.Stderr, "using cached release name %q for %s\n", record.Name, record.URL)
-			continue
-		}
-		_, _ = fmt.Fprintf(os.Stderr, "getting release name for %s\n", record.URL)
-		u, err := url.Parse(record.URL)
-		if err != nil {
-			panic(fmt.Errorf("failed to parse bosh release record url: %w", err))
-		}
-		var configFinal struct {
-			FinalName string `yaml:"final_name"`
-			Name      string `yaml:"name"`
-		}
-		err = getAndParse("https://"+path.Join("raw.githubusercontent.com", u.Path, "HEAD/config/final.yml"), &configFinal)
-		if err != nil {
-			panic(err)
-		}
-		if configFinal.FinalName != "" {
-			indexRecords[i].Name = configFinal.FinalName
-		} else {
-			indexRecords[i].Name = configFinal.Name
-		}
-	}
-
 	checkAndLogDuplicateNames(indexRecords)
 
 	result, err := yaml.Marshal(indexRecords)
@@ -86,38 +53,6 @@ func main() {
 	_, err = out.Write(result)
 	if err != nil {
 		panic(fmt.Errorf("failed to write to output: %w", err))
-	}
-}
-
-func getAndParse(uri string, data interface{}) error {
-	res, err := http.Get(uri)
-	if err != nil {
-		return fmt.Errorf("GET %s failed: %w", uri, err)
-	}
-	if res.StatusCode != http.StatusOK {
-		_ = res.Body.Close()
-		return fmt.Errorf("GET %s failed: expected status OK (200) got %s (%d)",
-			uri,
-			http.StatusText(res.StatusCode), res.StatusCode)
-	}
-	defer func() {
-		_ = res.Body.Close()
-	}()
-	err = yaml.NewDecoder(res.Body).Decode(data)
-	if err != nil {
-		return fmt.Errorf("failed to parse %s response: %w", uri, err)
-	}
-	return nil
-}
-
-func hydrateCache(previous, updated component.BoshReleaseRepositoryIndex) {
-	for i, ur := range updated {
-		for _, pr := range previous {
-			if ur.URL == pr.URL {
-				updated[i].Name = pr.Name
-				break
-			}
-		}
 	}
 }
 
