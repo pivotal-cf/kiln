14c8d9255d2273078b12590bcaff32da7d282325
hunterch <hunterch@vmware.com>
2022-08-11 11:03:43 -0700

    docs: tools package

--- internal/tools/import.go
+++ internal/tools/import.go
@@ -1,6 +1,8 @@
 //go:build tools
 // +build tools
 
+// Package tools is a convention in Go codebases to trick the dependency manager to include development CLIs into
+// the codebase. It is often used for generators.
 package tools
 
 import (
