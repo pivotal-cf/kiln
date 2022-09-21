8371c6fe488f5bf8afb8d258bccb0d08650fbbb5
hunterch <hunterch@vmware.com>
2022-09-21 10:56:38 -0700

    docs (fetchAssociatedStemcellVersion): failing step test with incorrect state
    this test is for ensuring the acceptance test steps are okay.
    If the test code skipped this step automatically, we would essentially be testing the step code twice.
    
    Once to make sure the state is setup then again in fetchAssociatedStemcellVersion to fetch the value from the same OM call.
    
    Co-authored-by: Ryan Hall <hallr@vmware.com>

--- internal/acceptance/workflows/scenario/ops_manager_environment_test.go
+++ internal/acceptance/workflows/scenario/ops_manager_environment_test.go
@@ -5,6 +5,8 @@ import (
 	"testing"
 )
 
+// Test_fetchAssociatedStemcellVersion will fail if you do not have a tile uploaded to the target ops manager
+// unset OM_TARGET when you want this test not to run.
 func Test_fetchAssociatedStemcellVersion(t *testing.T) {
 	_, err := loadEnvironmentVariable("OM_TARGET", "OM_TARGET not set")
 	if err != nil {
