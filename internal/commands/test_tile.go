5fabea211b3d17dd1c8c3d1d25a09b1517130872
Nick Rohn <nrohn@vmware.com>
2023-01-18 10:48:37 -0800

    wip: stream logs as they're written
    Authored-by: Nick Rohn <nrohn@vmware.com>

--- internal/commands/test_tile.go
+++ internal/commands/test_tile.go
@@ -1,13 +1,15 @@
 package commands
 
 import (
+	"bufio"
+	"bytes"
 	"context"
 	"fmt"
 	"github.com/docker/docker/api/types"
 	"github.com/docker/docker/api/types/container"
 	"github.com/docker/docker/api/types/mount"
 	"github.com/pivotal-cf/jhanda"
-	"io/ioutil"
+	"io"
 	"log"
 	"time"
 
@@ -43,7 +45,7 @@ func (u TestTile) Execute(args []string) error {
 
 	resp, err := cli.ContainerCreate(ctx, &container.Config{
 		Image: "gcr.io/tas-ppe/monorepo:latest",
-		Cmd:   []string{"/bin/bash", "-c", "cd /tas/test/stability && go env -w GO111MODULE=off && for i in $(seq 1 10); do go test ./; done"},
+		Cmd:   []string{"/bin/bash", "-c", "cd /tas/test/stability && go env -w GO111MODULE=off && for i in $(seq 1 3); do go test ./; sleep 1; done"},
 		Tty:   false,
 	}, &container.HostConfig{
 		Mounts: []mount.Mount{
@@ -62,37 +64,56 @@ func (u TestTile) Execute(args []string) error {
 		return err
 	}
 
-	fmt.Printf("<wait condition WaitConditionNotRunning> %s\n", time.Now())
-	statusCh, errCh := cli.ContainerWait(ctx, resp.ID, container.WaitConditionNotRunning)
-	var statusCode bool
-	select {
-	case err := <-errCh:
-		if err != nil {
-			return err
-		}
-	case status := <-statusCh:
-		statusCode = status.StatusCode != 1
-	}
+	//fmt.Printf("<wait condition WaitConditionNotRunning> %s\n", time.Now())
+	//statusCh, errCh := cli.ContainerWait(ctx, resp.ID, container.WaitConditionNotRunning)
+	//var statusCode bool
+	//select {
+	//case err := <-errCh:
+	//	if err != nil {
+	//		return err
+	//	}
+	//case status := <-statusCh:
+	//	statusCode = status.StatusCode != 1
+	//}
 
 	fmt.Printf("</wait condition WaitConditionNotRunning> %s\n\n", time.Now())
 	fmt.Printf("<container logs> %s\n", time.Now())
-	out, err := cli.ContainerLogs(ctx, resp.ID, types.ContainerLogsOptions{ShowStdout: true, ShowStderr: true})
+	out, err := cli.ContainerLogs(ctx, resp.ID, types.ContainerLogsOptions{ShowStdout: true, ShowStderr: true, Follow: true})
 	if err != nil {
 		return err
 	}
+	bufOut := new(bytes.Buffer)
+	w := bufio.NewWriter(bufOut)
+	buf := make([]byte, 1024)
+	for {
+		n, err := io.ReadAtLeast(out, buf, 1)
+		if err != nil && err != io.EOF {
+			return err
+		}
+		if n == 0 {
+			break
+		}
+		w.Write([]byte("line: "))
+		if _, err := w.Write(buf[:n]); err != nil {
+			return err
+		}
+		u.logger.Printf("line: %s", bufOut)
+		w.Flush()
+	}
+
 	fmt.Printf("</container logs> %s\n", time.Now())
 
-	b, err := ioutil.ReadAll(out)
-	if err != nil {
-		return err
-	}
-
-	u.logger.Print(string(b))
+	//b, err := ioutil.ReadAll(out)
+	//if err != nil {
+	//	return err
+	//}
+	//
+	//u.logger.Print(string(b))
 
 	timeout := time.Minute * 1
 	err = cli.ContainerStop(ctx, resp.ID, &timeout)
 
-	u.logger.Printf("test success: %t\n", statusCode)
+	//u.logger.Printf("test success: %t\n", statusCode)
 
 	return err
 
