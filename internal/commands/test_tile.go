771f603b3576482341737463fc1daffe7a67548e
Nick Rohn <nrohn@vmware.com>
2023-01-20 12:58:34 -0800

    wip: testing logging repetition

--- internal/commands/test_tile.go
+++ internal/commands/test_tile.go
@@ -50,6 +50,32 @@ func (statusState status) String() string {
 	}
 }
 
+// > one read per channel
+//   (lexically: yes)
+
+// option 1
+// one go routine to read logs
+//   for loop on logs, send to channel
+// on main thread, read the above channel / interrupt
+// see video
+
+// option 2 (most simple)
+// another way is to use signal with ctx. send to cnotainer ctx (signalwithctx)
+// ctx.err should have info after we finish. can check if err = cancelled
+// > keep track of the context tree so that cancels propagate to children correctly
+// > one way cancel -> parent to child
+
+// view chris' spike branch
+//   -> result of it lead to dockerization due to local ruby setup (yes)
+
+// use embed to ship assets
+// docs (blackfriday), dockerfile?, etc
+// nicer cli experience
+// > try to improve presentation along with flow
+// > consider hiding success and only logging failures
+
+// try using go run ginkgo instead of ginko directly
+
 func (u TestTile) Execute(args []string) error {
 	u.logger.Printf("Beginning stability tests for tas tile")
 	ctx := context.Background()
@@ -69,7 +95,7 @@ func (u TestTile) Execute(args []string) error {
 
 	resp, err := cli.ContainerCreate(ctx, &container.Config{
 		Image: "gcr.io/tas-ppe/monorepo:25c68328471ce80cbcbf4dfe8045b754019e2e3b",
-		Cmd:   []string{"/bin/bash", "-cx", "cd /tas/tas; for i in $(seq 1 10); do go test ./test/stability/; done; echo \"done\";"},
+		Cmd:   []string{"/bin/bash", "-cx", "cd /tas/tas; for i in $(seq 1 3); do echo \"<log $i>\"; go test ./test/stability/; echo \"</log $i>\"; done; echo \"done\";"},
 		Tty:   false,
 	}, &container.HostConfig{
 		Mounts: []mount.Mount{
@@ -107,7 +133,11 @@ func (u TestTile) Execute(args []string) error {
 	bufOut := new(bytes.Buffer)
 	w := bufio.NewWriter(bufOut)
 	buf := make([]byte, 1024)
+	//var readPointer int64 = 0
+	f, _ := os.Create("/tmp/testyboi")
+	f1, _ := os.OpenFile(f.Name(), os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0666)
 
+	wf := bufio.NewWriter(f1)
 readLogs:
 	for {
 		select {
@@ -116,6 +146,8 @@ readLogs:
 			break readLogs
 		default:
 			n, err := io.ReadAtLeast(out, buf, 1)
+
+			//io.NewSectionReader(out, readPointer, int64(n)-readPointer)
 			if err != nil && err != io.EOF {
 				interrupt <- syscall.SIGINT
 				return err
@@ -123,8 +155,16 @@ readLogs:
 			if n == 0 {
 				break readLogs
 			}
+			_, err = wf.Write([]byte(fmt.Sprintf("[%s] ", time.Now())))
+			if err != nil {
+				return err
+			}
 			_, err = w.Write([]byte(fmt.Sprintf("[%s] ", time.Now())))
 			if err != nil {
+				return err
+			}
+			if _, err := wf.Write(buf[:n]); err != nil {
+				interrupt <- syscall.SIGINT
 				return err
 			}
 			if _, err := w.Write(buf[:n]); err != nil {
@@ -133,6 +173,7 @@ readLogs:
 			}
 			u.logger.Printf("line: %s", bufOut)
 			err = w.Flush()
+			_ = wf.Flush()
 			if err != nil {
 				return err
 			}
