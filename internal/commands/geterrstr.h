dcae5ab4305757ac3b4d68cefff4d0889ff11aab
Christopher Hunter <hunterch@vmware.com>
2023-03-15 11:21:37 -0700

    refactor: remove development files
    per a conversation with Nick, these were accidently added during development

--- internal/commands/geterrstr.h
+++
@@ -1,9 +0,0 @@
-#ifndef _GETERRSTR_H
-#define _GETERRSTR_H
-
-#include <errno.h>
-#include <string.h>
-
-void geterrstr();
-
-#endif
--- internal/geterrstr.c
+++
@@ -1,5 +0,0 @@
-#include <errno.h>
-#include <string.h>
-void geterrstr() {
-    printf("%s\n", strerror(errno));
-}
