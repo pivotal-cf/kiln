e88dd3df1b6d63d197b7eedbd5309d8578c6cf45
Christopher Hunter <hunterch@vmware.com>
2023-05-08 12:38:13 -0700

    refactor(cargo): use table test for ReleaseSourceID

--- pkg/cargo/release_source_test.go
+++ pkg/cargo/release_source_test.go
@@ -6,81 +6,27 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestReleaseSourceID_zero(t *testing.T) {
-	assert.Equal(t, "", ReleaseSourceID(ReleaseSourceConfig{}))
-}
-
-func TestReleaseSourceID_id(t *testing.T) {
+func TestReleaseSourceID(t *testing.T) {
 	for _, tt := range []struct {
-		Type string
+		Name          string
+		ExpectedID    string
+		Configuration ReleaseSourceConfig
 	}{
-		{Type: ReleaseSourceTypeArtifactory},
-		{Type: ReleaseSourceTypeBOSHIO},
-		{Type: ReleaseSourceTypeGithub},
-		{Type: ReleaseSourceTypeS3},
+		{Name: "zero value", ExpectedID: "", Configuration: ReleaseSourceConfig{}},
+		{Name: "unknown type", ExpectedID: "", Configuration: ReleaseSourceConfig{Type: "banana"}},
+
+		{Name: ReleaseSourceTypeArtifactory + " with ID set", ExpectedID: "identifier", Configuration: ReleaseSourceConfig{ID: "identifier", Type: ReleaseSourceTypeArtifactory}},
+		{Name: ReleaseSourceTypeBOSHIO + " with ID set", ExpectedID: "identifier", Configuration: ReleaseSourceConfig{ID: "identifier", Type: ReleaseSourceTypeBOSHIO}},
+		{Name: ReleaseSourceTypeGithub + " with ID set", ExpectedID: "identifier", Configuration: ReleaseSourceConfig{ID: "identifier", Type: ReleaseSourceTypeGithub}},
+		{Name: ReleaseSourceTypeS3 + " with ID set", ExpectedID: "identifier", Configuration: ReleaseSourceConfig{ID: "identifier", Type: ReleaseSourceTypeS3}},
+
+		{Name: ReleaseSourceTypeArtifactory + " default", ExpectedID: ReleaseSourceTypeArtifactory, Configuration: ReleaseSourceConfig{ID: "", Type: ReleaseSourceTypeArtifactory}},
+		{Name: ReleaseSourceTypeBOSHIO + " default", ExpectedID: ReleaseSourceTypeBOSHIO, Configuration: ReleaseSourceConfig{ID: "", Type: ReleaseSourceTypeBOSHIO}},
+		{Name: ReleaseSourceTypeGithub + " default", ExpectedID: "identifier", Configuration: ReleaseSourceConfig{ID: "", Type: ReleaseSourceTypeGithub, Org: "identifier"}},
+		{Name: ReleaseSourceTypeS3 + " default", ExpectedID: "identifier", Configuration: ReleaseSourceConfig{ID: "", Type: ReleaseSourceTypeS3, Bucket: "identifier"}},
 	} {
-		t.Run(tt.Type, func(t *testing.T) {
-			configuration := releaseSourceWithAllFieldsSet("x")
-			configuration.Type = tt.Type
-			configuration.ID = "identifier"
-			id := ReleaseSourceID(configuration)
-			assert.Equal(t, "identifier", id)
+		t.Run(tt.Name, func(t *testing.T) {
+			assert.Equal(t, tt.ExpectedID, ReleaseSourceID(tt.Configuration))
 		})
 	}
-}
-
-func TestReleaseSourceID_artifactory(t *testing.T) {
-	configuration := releaseSourceWithAllFieldsSet("x")
-	configuration.Type = ReleaseSourceTypeArtifactory
-	configuration.ID = ""
-	defaultReleaseSourceIDIsType(t, configuration)
-}
-
-func TestReleaseSourceID_bosh(t *testing.T) {
-	configuration := releaseSourceWithAllFieldsSet("x")
-	configuration.Type = ReleaseSourceTypeBOSHIO
-	configuration.ID = ""
-	defaultReleaseSourceIDIsType(t, configuration)
-}
-
-func TestReleaseSourceID_github(t *testing.T) {
-	configuration := releaseSourceWithAllFieldsSet("x")
-	configuration.Type = ReleaseSourceTypeGithub
-	configuration.ID = ""
-	configuration.Org = "identifier"
-	assert.Equal(t, "identifier", ReleaseSourceID(configuration))
-}
-
-func TestReleaseSourceID_s3(t *testing.T) {
-	configuration := releaseSourceWithAllFieldsSet("x")
-	configuration.Type = ReleaseSourceTypeS3
-	configuration.ID = ""
-	configuration.Bucket = "identifier"
-	assert.Equal(t, "identifier", ReleaseSourceID(configuration))
-}
-
-func releaseSourceWithAllFieldsSet(value string) ReleaseSourceConfig {
-	return ReleaseSourceConfig{
-		Type:            value,
-		ID:              value,
-		Publishable:     true,
-		Bucket:          value,
-		Region:          value,
-		AccessKeyId:     value,
-		SecretAccessKey: value,
-		PathTemplate:    value,
-		Endpoint:        value,
-		Org:             value,
-		GithubToken:     value,
-		Repo:            value,
-		ArtifactoryHost: value,
-		Username:        value,
-		Password:        value,
-	}
-}
-
-func defaultReleaseSourceIDIsType(t *testing.T, configuration ReleaseSourceConfig) {
-	t.Helper()
-	id := ReleaseSourceID(configuration)
-	assert.Equal(t, configuration.Type, id)
 }
