40e962f7443836977ab34f4705ba44df627a88e3
Christopher Hunter <hunterch@vmware.com>
2023-05-25 11:01:50 -0700

    Revert "refactor: move breaking changes detector to separate package"
    This reverts commit 9f6163aa4099e861d2d5289a8b4cb90814d71e11.

--- pkg/proofing/instance_definition.go
+++ pkg/proofing/instance_definition.go
@@ -1,10 +1,10 @@
 package proofing
 
 type InstanceDefinition struct {
-	Default      int                 `yaml:"default"`
-	Configurable bool                `yaml:"configurable"`
-	Constraints  *IntegerConstraints `yaml:"constraints,omitempty"`
-	ZeroIf       ZeroIfBinding       `yaml:"zero_if,omitempty"` // TODO: schema?
+	Default      int                `yaml:"default"`
+	Configurable bool               `yaml:"configurable"`
+	Constraints  IntegerConstraints `yaml:"constraints,omitempty"`
+	ZeroIf       ZeroIfBinding      `yaml:"zero_if,omitempty"` // TODO: schema?
 
 	// TODO: validations: https://github.com/pivotal-cf/installation/blob/039a2ef3f751ef5915c425da8150a29af4b764dd/web/app/models/persistence/metadata/instance_definition.rb#L9-L12
 }
