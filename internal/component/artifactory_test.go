41bff4c4b987335963f0963d9c13c9e58096b259
Christopher Hunter <hunterch@vmware.com>
2023-05-31 16:36:25 -0700

    refactor(component): add coverage for artifactory release source behavior

---
+++ internal/component/artifactory_test.go
@@ -0,0 +1,181 @@
+package component_test
+
+import (
+	"io"
+	"log"
+	"net/http"
+	"net/http/httptest"
+	"os"
+	"path/filepath"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	"github.com/pivotal-cf/kiln/internal/component"
+	"github.com/pivotal-cf/kiln/pkg/cargo"
+)
+
+var _ = Describe("interacting with BOSH releases on Artifactory", func() {
+	var (
+		source  *component.ArtifactoryReleaseSource
+		config  cargo.ReleaseSourceConfig
+		server  *httptest.Server
+		handler http.Handler
+
+		releasesDirectory string
+	)
+	BeforeEach(func() {
+		source = new(component.ArtifactoryReleaseSource)
+		releasesDirectory = os.TempDir()
+		config = cargo.ReleaseSourceConfig{}
+	})
+	JustBeforeEach(func() {
+		server = httptest.NewServer(handler)
+		config.ArtifactoryHost = server.URL
+		source = component.NewArtifactoryReleaseSource(config)
+	})
+	AfterEach(func() {
+		server.Close()
+		_ = os.RemoveAll(releasesDirectory)
+	})
+
+	Describe("read operations", func() {
+		When("the server has the a file at the expected path", func() {
+			BeforeEach(func() {
+				const (
+					correctUsername = "kim"
+					correctPassword = "mango_rice!"
+				)
+
+				config.Repo = "basket"
+				config.PathTemplate = "bosh-releases/{{.StemcellOS}}/{{.StemcellVersion}}/{{.Name}}/{{.Name}}-{{.Version}}-{{.StemcellOS}}-{{.StemcellVersion}}.tgz"
+				config.Username = correctUsername
+				config.Password = correctPassword
+				config.ID = "some-mango-tree"
+
+				requireAuth := requireBasicAuthMiddleware(correctUsername, correctPassword)
+
+				mux := http.NewServeMux()
+				mux.HandleFunc("/", func(res http.ResponseWriter, req *http.Request) {
+					res.Header().Set("content-type", "text/plain")
+					GinkgoT().Logf("handler for request not found: %s %s", req.Method, req.URL)
+					http.Error(res, http.StatusText(http.StatusNotFound), http.StatusNotFound)
+				})
+				mux.Handle("/api/storage/basket/bosh-releases/smoothie/9.9/mango/mango-2.3.4-smoothie-9.9.tgz", applyMiddleware(http.HandlerFunc(func(res http.ResponseWriter, _ *http.Request) {
+					res.WriteHeader(http.StatusOK)
+					// language=json
+					_, _ = io.WriteString(res, `{"checksums": {"sha1":  "some-sha"}}`)
+				})))
+				mux.Handle("/api/storage/basket/bosh-releases/smoothie/9.9/mango", applyMiddleware(http.HandlerFunc(func(res http.ResponseWriter, _ *http.Request) {
+					res.WriteHeader(http.StatusOK)
+					// language=json
+					_, _ = io.WriteString(res, `{"children": [{"uri": "/mango-2.3.4-smoothie-9.9.tgz", "folder": false}]}`)
+				}), requireAuth))
+				mux.Handle("/artifactory/basket/bosh-releases/smoothie/9.9/mango/mango-2.3.4-smoothie-9.9.tgz", applyMiddleware(http.HandlerFunc(func(res http.ResponseWriter, _ *http.Request) {
+					res.WriteHeader(http.StatusOK)
+					f, err := os.Open(filepath.Join("testdata", "some-release.tgz"))
+					if err != nil {
+						log.Fatal("failed to open some release test artifact")
+					}
+					defer closeAndIgnoreError(f)
+					_, _ = io.Copy(res, f)
+				}) /* put middleware here */))
+				handler = mux
+			})
+
+			It("resolves the lock from the spec", func() { // testing GetMatchedRelease
+				source.ID = "some-mango-tree"
+				source.ReleaseSourceConfig.ID = "why-are-we-not-using-this-field"
+				resultLock, resultErr := source.GetMatchedRelease(component.Spec{
+					Name:            "mango",
+					Version:         "2.3.4",
+					StemcellOS:      "smoothie",
+					StemcellVersion: "9.9",
+				})
+
+				Expect(resultErr).NotTo(HaveOccurred())
+				Expect(resultLock).To(Equal(component.Lock{
+					Name:    "mango",
+					Version: "2.3.4",
+					//StemcellOS:      "smoothie",
+					//StemcellVersion: "9.9",
+					RemotePath:   "bosh-releases/smoothie/9.9/mango/mango-2.3.4-smoothie-9.9.tgz",
+					RemoteSource: "some-mango-tree",
+				}))
+			})
+
+			It("finds the bosh release", func() { // testing FindReleaseVersion
+				resultLock, resultErr := source.FindReleaseVersion(component.Spec{
+					Name:            "mango",
+					Version:         "2.3.4",
+					StemcellOS:      "smoothie",
+					StemcellVersion: "9.9",
+				}, false)
+
+				Expect(resultErr).NotTo(HaveOccurred())
+				Expect(resultLock).To(Equal(component.Lock{
+					Name:    "mango",
+					Version: "2.3.4",
+					//StemcellOS:      "smoothie",
+					//StemcellVersion: "9.9",
+					SHA1:         "some-sha",
+					RemotePath:   "bosh-releases/smoothie/9.9/mango/mango-2.3.4-smoothie-9.9.tgz",
+					RemoteSource: "some-mango-tree",
+				}))
+			})
+
+			It("downloads the release", func() { // testing FindReleaseVersion
+				source.ID = "some-mango-tree"
+				source.ReleaseSourceConfig.ID = "why-are-we-not-using-this-field"
+
+				local, resultErr := source.DownloadRelease(releasesDirectory, component.Lock{
+					Name:         "mango",
+					Version:      "2.3.4",
+					RemotePath:   "bosh-releases/smoothie/9.9/mango/mango-2.3.4-smoothie-9.9.tgz",
+					RemoteSource: "some-mango-tree",
+				})
+
+				Expect(resultErr).NotTo(HaveOccurred())
+				Expect(local.LocalPath).To(BeAnExistingFile())
+			})
+		})
+	})
+	Describe("ReleaseUploader", func() {
+		Describe("UploadRelease", func() {
+
+		})
+	})
+})
+
+func closeAndIgnoreError(c io.Closer) {
+	_ = c.Close()
+}
+
+func requireBasicAuthMiddleware(expectedUsername, expectedPassword string) func(http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(res http.ResponseWriter, req *http.Request) {
+			username, password, ok := req.BasicAuth()
+			if !ok {
+				http.Error(res, "auth not set", http.StatusUnauthorized)
+				return
+			}
+			if expectedUsername != username {
+				http.Error(res, "username does not match", http.StatusUnauthorized)
+				return
+			}
+			if expectedPassword != password {
+				http.Error(res, "password does not match", http.StatusUnauthorized)
+				return
+			}
+			next.ServeHTTP(res, req)
+		})
+	}
+}
+
+func applyMiddleware(endpoint http.Handler, middleware ...func(http.Handler) http.Handler) http.Handler {
+	h := endpoint
+	for _, mw := range middleware {
+		h = mw(h)
+	}
+	return h
+}
