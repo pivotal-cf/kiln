73431b97119bcae91bfb0e6ecb4b935ad58a4600
Christopher Hunter <hunterch@vmware.com>
2023-05-31 21:24:57 -0700

    wip: add scratch pad for testing

---
+++ internal/component/scratchpad_test.go
@@ -0,0 +1,87 @@
+package component
+
+import (
+	"net/http"
+	"os"
+	"path/filepath"
+	"testing"
+
+	"github.com/pivotal-cf/kiln/pkg/cargo"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+// TODO: delete this test before merging to main
+func TestArtifactoryAgainstTheRealServer(t *testing.T) {
+	artifactoryHost := "https://artifactory.eng.vmware.com"
+	_, err := http.Get(artifactoryHost)
+	if err != nil {
+		t.Skip(err)
+	}
+	source := NewArtifactoryReleaseSource(cargo.ReleaseSourceConfig{
+		ArtifactoryHost: artifactoryHost,
+		ID:              "shared-releases-ubuntu",
+		Repo:            "tanzu-application-services-generic-local",
+		Publishable:     true,
+		PathTemplate:    "shared-releases/{{.StemcellOS}}/{{.StemcellVersion}}/{{.Name}}/{{.Name}}-{{.Version}}-{{.StemcellOS}}-{{.StemcellVersion}}.tgz",
+
+		Username: "",
+		Password: "",
+	})
+
+	t.Run("GetMatchedRelease", func(t *testing.T) {
+		t.Parallel()
+		lock, err := source.GetMatchedRelease(Spec{
+			Name:            "bpm",
+			Version:         "1.2.2",
+			StemcellVersion: "1.117",
+			StemcellOS:      "ubuntu-jammy",
+		})
+		require.NoError(t, err)
+		assert.Equal(t, Lock{
+			Name:         "bpm",
+			SHA1:         "551a2900eb32b246c8dead5bd5be749c47fce588",
+			Version:      "1.2.2",
+			RemoteSource: "shared-releases-ubuntu",
+			RemotePath:   "shared-releases/ubuntu-jammy/1.117/bpm/bpm-1.2.2-ubuntu-jammy-1.117.tgz",
+		}, lock)
+	})
+
+	t.Run("FindReleaseVersion", func(t *testing.T) {
+		t.Parallel()
+		lock, err := source.FindReleaseVersion(Spec{
+			Name:            "bpm",
+			Version:         "*",
+			StemcellVersion: "1.117",
+			StemcellOS:      "ubuntu-jammy",
+		}, false)
+		require.NoError(t, err)
+		assert.Equal(t, Lock{
+			Name:            "bpm",
+			SHA1:            "551a2900eb32b246c8dead5bd5be749c47fce588",
+			Version:         "1.2.2",
+			StemcellOS:      "ubuntu-jammy",
+			StemcellVersion: "1.117",
+			RemoteSource:    "shared-releases-ubuntu",
+			RemotePath:      "shared-releases/ubuntu-jammy/1.117/bpm/bpm-1.2.2-ubuntu-jammy-1.117.tgz",
+		}, lock)
+	})
+
+	t.Run("DownloadRelease", func(t *testing.T) {
+		t.Parallel()
+		dir := filepath.Join(os.TempDir(), "kiln-DownloadRelease")
+		require.NoError(t, os.MkdirAll(dir, 0744))
+
+		local, err := source.DownloadRelease(dir, Lock{
+			Name:         "bpm",
+			SHA1:         "551a2900eb32b246c8dead5bd5be749c47fce588",
+			Version:      "1.2.2",
+			RemoteSource: "shared-releases-ubuntu",
+			RemotePath:   "shared-releases/ubuntu-jammy/1.117/bpm/bpm-1.2.2-ubuntu-jammy-1.117.tgz",
+		})
+		require.NoError(t, err)
+		t.Logf("%#v", local)
+	})
+
+	t.Error("this should not be on main")
+}
