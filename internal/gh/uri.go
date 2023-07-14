53f0cca6306d04e1a8ea21843caeb17cad3fd7c1
Christopher Hunter <hunterch@vmware.com>
2023-07-14 16:14:54 -0400

    refactor(gh): remove useless defer

--- internal/gh/uri.go
+++ internal/gh/uri.go
@@ -1,7 +1,6 @@
 package gh
 
 import (
-	"errors"
 	"fmt"
 	"net/url"
 	"path/filepath"
@@ -11,22 +10,20 @@ import (
 // RepositoryOwnerAndNameFromPath is from the github-release-source branch
 // once that one is merged we should that one instead of this one
 func RepositoryOwnerAndNameFromPath(urlStr string) (owner, repo string, err error) {
-	defer func() {
-		if err != nil {
-			err = fmt.Errorf("failed to parse owner and repo name from URI %q: %w", urlStr, err)
-		}
-	}()
+	wrapError := func(urlStr string, err error) error {
+		return fmt.Errorf("failed to parse owner and repo name from URI %q: %w", urlStr, err)
+	}
 	urlStr = strings.TrimPrefix(urlStr, "git@github.com:")
 	u, err := url.Parse(urlStr)
 	if err != nil {
-		return "", "", err
+		return "", "", wrapError(urlStr, err)
 	}
 	if filepath.Ext(u.Path) == ".git" {
 		u.Path = strings.TrimSuffix(u.Path, ".git")
 	}
 	owner, repo, found := strings.Cut(strings.TrimPrefix(u.Path, "/"), "/")
 	if !found || owner == "" || repo == "" {
-		return owner, repo, errors.New("path missing expected parts")
+		return owner, repo, wrapError(urlStr, fmt.Errorf("path missing expected parts"))
 	}
 	return owner, repo, nil
 }
