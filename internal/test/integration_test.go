fc4709502d44f22be723f9ad61a1bd09cecd0b51
Christopher Hunter <hunterch@vmware.com>
2023-07-18 12:08:11 -0400

    fix: add test case for manifest test failure

--- internal/test/integration_test.go
+++ internal/test/integration_test.go
@@ -1,14 +1,14 @@
 package test_test
 
 import (
+	"bytes"
 	"context"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 	"io"
 	"os"
 	"path/filepath"
 	"testing"
-
-	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/require"
 
 	"github.com/pivotal-cf/kiln/internal/commands"
 	"github.com/pivotal-cf/kiln/internal/test"
@@ -17,24 +17,53 @@ import (
 var _ commands.TileTestFunction = test.Run
 
 func TestDockerIntegration(t *testing.T) {
-	_, githubWorkspaceEnvVarFound := os.LookupEnv("GITHUB_WORKSPACE")
-	if testing.Short() || githubWorkspaceEnvVarFound {
-		t.Skip("integration test is slow")
-	}
+	t.Run("the test succeeds", func(t *testing.T) {
+		_, githubWorkspaceEnvVarFound := os.LookupEnv("GITHUB_WORKSPACE")
+		if testing.Short() || githubWorkspaceEnvVarFound {
+			t.Skip("integration test is slow")
+		}
 
-	wd, err := os.Getwd()
-	require.NoError(t, err)
+		wd, err := os.Getwd()
+		require.NoError(t, err)
 
-	ctx := context.Background()
-	configuration := test.Configuration{
-		AbsoluteTileDirectory: filepath.Join(wd, "testdata", "happy-tile"),
-		RunAll:                true,
-	}
-	out := io.Discard
-	if testing.Verbose() {
-		out = os.Stdout
-	}
+		ctx := context.Background()
+		configuration := test.Configuration{
+			AbsoluteTileDirectory: filepath.Join(wd, "testdata", "happy-tile"),
+			RunAll:                true,
+		}
+		out := io.Discard
+		if testing.Verbose() {
+			out = os.Stdout
+		}
 
-	err = test.Run(ctx, out, configuration)
-	assert.NoError(t, err)
+		err = test.Run(ctx, out, configuration)
+		assert.NoError(t, err)
+	})
+
+	t.Run("the test fails", func(t *testing.T) {
+		_, githubWorkspaceEnvVarFound := os.LookupEnv("GITHUB_WORKSPACE")
+		if testing.Short() || githubWorkspaceEnvVarFound {
+			t.Skip("integration test is slow")
+		}
+
+		wd, err := os.Getwd()
+		require.NoError(t, err)
+
+		ctx := context.Background()
+		configuration := test.Configuration{
+			AbsoluteTileDirectory: filepath.Join(wd, "testdata", "happy-tile"),
+			RunManifest:           true,
+			Environment:           []string{"FAIL_TEST=true"},
+		}
+
+		outBuffer := new(bytes.Buffer)
+		var out io.Writer = outBuffer
+		if testing.Verbose() {
+			out = io.MultiWriter(outBuffer, os.Stdout)
+		}
+
+		err = test.Run(ctx, out, configuration)
+		assert.Error(t, err)
+		assert.Contains(t, outBuffer.String(), "FAIL_TEST is true")
+	})
 }
