beada0202ad07dc6b98d4ea02fbd5be927fae017
Christopher Hunter <hunterch@vmware.com>
2023-08-08 16:38:49 -0700

    refactor: simplify git_metadata_sha propigation
    this refactor is in reponse to the Slack comment "this resilient enough to lots of things that might go wrong?"
    reffering to the previous GitMetadataSHA implementation
    
    I agree it was not simple. I hope this is better.

--- internal/builder/metadata_git_sha.go
+++ internal/builder/metadata_git_sha.go
@@ -10,34 +10,38 @@ import (
 
 const dirtyStateSHAValue = "DEVELOPMENT"
 
-func GitMetadataSHA(repositoryDirectory string, isDev bool) func() (string, error) {
-	var cache string
-	return func() (s string, err error) {
-		if cache != "" {
-			return cache, nil
-		}
-		if _, err := exec.LookPath("git"); err != nil {
-			return "", fmt.Errorf("could not calculate %q: %w", MetadataGitSHAVariable, err)
-		}
-		gitStatus := exec.Command("git", "status", "--porcelain")
-		gitStatus.Dir = repositoryDirectory
-		err = gitStatus.Run()
-		if err != nil {
-			if gitStatus.ProcessState.ExitCode() == 1 && isDev {
-				_, _ = fmt.Fprintf(os.Stderr, "WARNING: git working directory has un-commited changes: the variable %q has has development only value %q", MetadataGitSHAVariable, dirtyStateSHAValue)
-				return "DEVELOPMENT", nil
-			}
-			return "", fmt.Errorf("failed to run `%s %s`: %w", gitStatus.Path, strings.Join(gitStatus.Args, " "), err)
-		}
-		var out bytes.Buffer
-		gitRevParseHead := exec.Command("git", "rev-parse", "HEAD")
-		gitRevParseHead.Dir = repositoryDirectory
-		gitRevParseHead.Stdout = &out
-		err = gitRevParseHead.Run()
-		if err != nil {
-			return "", fmt.Errorf("failed to get HEAD revision hash: %w", err)
-		}
-		cache = strings.TrimSpace(out.String())
-		return cache, nil
+func GitMetadataSHA(repositoryDirectory string, isDev bool) (string, error) {
+	if err := ensureGitExecutableIsFound(); err != nil {
+		return "", err
 	}
+	gitStatus := exec.Command("git", "status", "--porcelain")
+	gitStatus.Dir = repositoryDirectory
+	err := gitStatus.Run()
+	if err != nil {
+		if gitStatus.ProcessState.ExitCode() == 1 && isDev {
+			_, _ = fmt.Fprintf(os.Stderr, "WARNING: git working directory has un-commited changes: the variable %q has has development only value %q", MetadataGitSHAVariable, dirtyStateSHAValue)
+			return dirtyStateSHAValue, nil
+		}
+		return "", fmt.Errorf("failed to run `%s %s`: %w", gitStatus.Path, strings.Join(gitStatus.Args, " "), err)
+	}
+	return gitHeadRevision(repositoryDirectory)
+}
+
+func gitHeadRevision(repositoryDirectory string) (string, error) {
+	var out bytes.Buffer
+	gitRevParseHead := exec.Command("git", "rev-parse", "HEAD")
+	gitRevParseHead.Dir = repositoryDirectory
+	gitRevParseHead.Stdout = &out
+	err := gitRevParseHead.Run()
+	if err != nil {
+		return "", fmt.Errorf("failed to get HEAD revision hash: %w", err)
+	}
+	return strings.TrimSpace(out.String()), nil
+}
+
+func ensureGitExecutableIsFound() error {
+	if _, err := exec.LookPath("git"); err != nil {
+		return fmt.Errorf("could not calculate %q: %w", MetadataGitSHAVariable, err)
+	}
+	return nil
 }
