4fe3e7aa9925a6a42e839f672a6af405291ac8c0
Christopher Hunter <hunterch@vmware.com>
2023-09-05 12:58:53 -0700

    refactor(test): fail when docker daemon is not new enough
    this should help debugging kiln issues
    I thought there was a problem with kiln when it was just my machine

--- internal/test/integration_test.go
+++ internal/test/integration_test.go
@@ -11,6 +11,9 @@ import (
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 
+	"github.com/Masterminds/semver/v3"
+	"github.com/docker/docker/client"
+
 	"github.com/pivotal-cf/kiln/internal/commands"
 	"github.com/pivotal-cf/kiln/internal/test"
 )
@@ -18,6 +21,8 @@ import (
 var _ commands.TileTestFunction = test.Run
 
 func TestDockerIntegration(t *testing.T) {
+	checkDaemonVersion(t)
+
 	t.Run("the test succeeds", func(t *testing.T) {
 		_, githubWorkspaceEnvVarFound := os.LookupEnv("GITHUB_WORKSPACE")
 		if testing.Short() || githubWorkspaceEnvVarFound {
@@ -67,4 +72,26 @@ func TestDockerIntegration(t *testing.T) {
 		assert.Error(t, err)
 		assert.Contains(t, outBuffer.String(), "FAIL_TEST is true")
 	})
+}
+
+func checkDaemonVersion(t *testing.T) {
+	t.Helper()
+
+	constraints, err := semver.NewConstraint(test.MinimumDockerServerVersion)
+	require.NoError(t, err)
+
+	dockerClient, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
+	require.NoError(t, err)
+	ctx := context.Background()
+
+	info, err := dockerClient.ServerVersion(ctx)
+	require.NoError(t, err)
+
+	v := semver.MustParse(info.Version)
+
+	ok, reasons := constraints.Validate(v)
+	for _, reason := range reasons {
+		t.Log(reason.Error())
+	}
+	require.True(t, ok, "kiln test requires a newer version of Docker")
 }
