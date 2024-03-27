6e4514129015fce4432258c61f95ac15df9d187f
Christopher Hunter <8398225+crhntr@users.noreply.github.com>
2024-03-26 20:00:53 -0700

    add "bake.New" function to split a into defaults based tile source

---
+++ pkg/bake/defaults.go
@@ -0,0 +1,17 @@
+package bake
+
+const (
+	DefaultFilepathIconImage    = "icon.png"
+	DefaultFilepathKilnfile     = "Kilnfile"
+	DefaultFilepathKilnfileLock = DefaultFilepathKilnfile + ".lock"
+	DefaultFilepathBaseYML      = "base.yml"
+
+	DefaultDirectoryReleases       = "releases"
+	DefaultDirectoryForms          = "forms"
+	DefaultDirectoryInstanceGroups = "instance_groups"
+	DefaultDirectoryJobs           = "jobs"
+	DefaultDirectoryMigrations     = "migrations"
+	DefaultDirectoryProperties     = "properties"
+	DefaultDirectoryRuntimeConfigs = "runtime_configs"
+	DefaultDirectoryBOSHVariables  = "bosh_variables"
+)
