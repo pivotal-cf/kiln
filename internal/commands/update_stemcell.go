f362efc0a3a1dfe47fb1b4c48ca9e4773cc8e451
Daniel Linsley <daniel.linsley@broadcom.com>
2024-07-01 17:18:17 +0000

    do not download release for update stemcell unless required

--- internal/commands/update_stemcell.go
+++ internal/commands/update_stemcell.go
@@ -18,9 +18,8 @@ type UpdateStemcell struct {
 	Options struct {
 		flags.Standard
 
-		Version         string `short:"v"  long:"version"            required:"true"    description:"desired version of stemcell"`
-		ReleasesDir     string `short:"rd" long:"releases-directory" default:"releases" description:"path to a directory to download releases into"`
-		WithoutDownload bool   `long:"without-download" description:"updates stemcell releases without downloading releases"`
+		Version     string `short:"v"  long:"version"            required:"true"    description:"desired version of stemcell"`
+		ReleasesDir string `short:"rd" long:"releases-directory" default:"releases" description:"path to a directory to download releases into"`
 	}
 	FS                         billy.Filesystem
 	MultiReleaseSourceProvider MultiReleaseSourceProvider
@@ -92,14 +91,15 @@ func (update UpdateStemcell) Execute(args []string) error {
 		}
 		lock := &kilnfileLock.Releases[i]
 
-		if update.Options.WithoutDownload {
-			lock.SHA1 = remote.SHA1
-		} else {
+		if remote.SHA1 == "" || remote.SHA1 == "not-calculated" {
+			// release source needs to download.
 			local, err := releaseSource.DownloadRelease(update.Options.ReleasesDir, remote)
 			if err != nil {
 				return fmt.Errorf("while downloading release %q, encountered error: %w", rel.Name, err)
 			}
 			lock.SHA1 = local.Lock.SHA1
+		} else {
+			lock.SHA1 = remote.SHA1
 		}
 		lock.RemotePath = remote.RemotePath
 		lock.RemoteSource = remote.RemoteSource
